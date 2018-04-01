0a73027bcdb9eaf615bba275b7a4fd876da3b656
Unknown <tanks.co@outlook.com>
2018-04-01 20:10:32 +0100

    Added Blasted Rock and Burial Mounds.
    Pushed for the GitKraken Guide.

---
+++ maps/blasted 20/blasted_20_scenario.lua
@@ -0,0 +1,41 @@
+version = 3
+
+ScenarioInfo = {
+  map_version = 4,
+  name = 'Blasted Rock 20 (8 Player)',
+  type = 'skirmish',
+  description = '<LOC Blasted_20_Description>The former mining colony at Blasted Rock having been wiped out, the conflict widens....',
+  starts = true,
+  preview = '',
+  size = {1024, 1024},
+  norushradius = 36,
+  map = '/maps/Blasted_20/Blasted_20.scmap',
+  save = '/maps/Blasted_20/Blasted_20_save.lua',
+  script = '/maps/Blasted_20/Blasted_20_script.lua',
+  Configurations = {
+    ['standard'] = {
+      teams = {
+        {
+          name = 'FFA',
+          armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6', 'ARMY_7', 'ARMY_8'},
+        },
+      },
+    },
+  },
+  norushoffsetX_ARMY_1 = 0,
+  norushoffsetY_ARMY_1 = 0,
+  norushoffsetX_ARMY_2 = 0,
+  norushoffsetY_ARMY_2 = 0,
+  norushoffsetX_ARMY_3 = 0,
+  norushoffsetY_ARMY_3 = 0,
+  norushoffsetX_ARMY_4 = 0,
+  norushoffsetY_ARMY_4 = 0,
+  norushoffsetX_ARMY_5 = 0,
+  norushoffsetY_ARMY_5 = 0,
+  norushoffsetX_ARMY_6 = 0,
+  norushoffsetY_ARMY_6 = 0,
+  norushoffsetX_ARMY_7 = 0,
+  norushoffsetY_ARMY_7 = 0,
+  norushoffsetX_ARMY_8 = 0,
+  norushoffsetY_ARMY_8 = 0,
+}
