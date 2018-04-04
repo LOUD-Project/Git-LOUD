9a70823dfd37ed45ae723de104f30abcd1ed513f
Sean Wheeldon <the.balthazar@gmail.com>
2018-04-04 20:44:08 +0100

    Update ZZZ0002_script.lua
    Fixed relative link to old directory structure.

--- mods/logger/units/ZZZ0002/ZZZ0002_script.lua
+++ mods/logger/units/ZZZ0002/ZZZ0002_script.lua
@@ -5,7 +5,7 @@ ZZZ0002 = Class(AShieldStructureUnit) {
         AShieldStructureUnit.OnCreate(self)
         self.DomeEntity = import('/lua/sim/Entity.lua').Entity({Owner = self,})
         self.DomeEntity:AttachBoneTo( -1, self, 0 )
-        self.DomeEntity:SetMesh('/mods/brewlan_plenae/logger/units/zzz0002/zzz0002_domes_mesh')
+        self.DomeEntity:SetMesh('/mods/logger/units/zzz0002/zzz0002_domes_mesh')
         self.DomeEntity:SetDrawScale(self:GetBlueprint().Display.UniformScale)
         self.DomeEntity:SetVizToAllies('Intel')
         self.DomeEntity:SetVizToNeutrals('Intel')
