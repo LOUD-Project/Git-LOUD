85aaf3fe361a1edd151ee76e985fc10b1c6193fd
Sprouto <loudman@rogers.com>
2018-04-12 22:58:26 -0400

    Further Restrictions work
    Added tooltips and a couple more breakout restrictions - seperated T3 and Experimental artillery from GameEnders
    
    Was studying some UI code since rollover descriptions don't seem to be the same width as Rollover Titles.

--- gamedata/03_LobbyEnhancement/lua/LEMLobbyOptions.lua
+++ gamedata/03_LobbyEnhancement/lua/LEMLobbyOptions.lua
@@ -43,31 +43,31 @@ GPGrestrictedUnits = {
     AIRSCOUTS = {
         categories = {"uaa0101","uaa0302","uea0101","uea0302","ura0101","ura0302","xsa0101","xsa0302" },
         name = "No Air Scouts",
-        tooltip = "No Air Scouts or Spy Planes",
+        tooltip = "restricted_units_air_scouts",
     },
 
     AIRFIGHTERS = {
 		categories = {"brpat2figbo","uaa0102","uea0102","ura0102","xsa0102","xaa0202","dea0202","dra0202","xsa0202","uaa0303","uea0303","ura0303","xsa0303"},
 		name = "No Air Fighters",
-		tooltip = "No Air Fighters",
+		tooltip = "restricted_units_air_fighters",
     },
 	
     AIRBOMBERS = {
 		categories = {"BOMBER","TORPEDOBOMBER"},
 		name = "No Air Bombers or Torpedo Bombers",
-		tooltip = "No Bombers or Torpedo Bombers",
+		tooltip = "restricted_units_air_bombers",
     },
 	
     AIRGUNSHIPS = {
 		categories = {"GROUNDATTACK"},
-		name = "No Gunships",
-		tooltip = "No Gunships",
+		name = "No Air Gunships",
+		tooltip = "restricted_units_air_gunships",
     },
 	
     AIRTRANSPORTS = {
 		categories = {"TRANSPORTFOCUS"},
 		name = "No Air Transports",
-		tooltip = "No Air Transports",
+		tooltip = "restricted_units_air_transports",
     },
 
     AIREXPERIMENTALS = {
@@ -76,51 +76,39 @@ GPGrestrictedUnits = {
 		tooltip = "restricted_units_air_experimentals",
     },
 	
-    UEF = {
-        categories = {"UEF"},
-        name = "<LOC restricted_units_data_0007>No UEF",
-        tooltip = "restricted_units_uef",
-    },
-	
-    CYBRAN = {
-        categories = {"CYBRAN"},
-        name = "<LOC restricted_units_data_0008>No Cybran",
-        tooltip = "restricted_units_cybran",
-    },
-	
-    AEON = {
-        categories = {"AEON"},
-        name = "<LOC restricted_units_data_0009>No Aeon",
-        tooltip = "restricted_units_aeon",
-    },
-	
-    SERAPHIM = {
-        categories = {"SERAPHIM"},
-        name = "<LOC restricted_units_data_0010>No Seraphim",
-        tooltip = "restricted_units_seraphim",
-    },
-	
 	TACTICALMISSILELAUNCHERS = {
 		categories = { 'uab2108', 'ueb2108', 'urb2108', 'xsb2108', 'bab2308', },
-		name = "No Tactical Missile Launcher structures",
+		name = "No Tactical Missile structures",
 		tooltip = "restricted_units_tactical",
 	},
 	
     NUKE = {
-        categories = {"uab2305", "ueb2305", "urb2305", "xsb2305", "xsb2401", "uas0304", "ues0304", "urs0304", },
+        categories = {"uab2305", "ueb2305", "urb2305", "xsb2305", "xsb2401", "uas0304", "ues0304", "urs0304", "xss0302" },
         name = "<LOC restricted_units_data_0011>No Nukes",
         tooltip = "restricted_units_nukes",
     },
 	
+	HEAVYARTILLERY = {
+		categories = { "uab2302","ueb2302","urb2302","xsb2302" },
+		name = "No T3 Artillery",
+		tooltip = "restricted_units_T3_artillery",
+	},
+	
+	EXPERIMENTALARTILLERY = {
+		categories = { "xab2307", "ueb2401", "url0401" },
+		name = "No Experimental Artillery",
+		tooltip = "restricted_units_exp_artillery",
+	},
+	
     GAMEENDERS = {
-        categories = {"uab2305", "ueb2302", "ueb2305", "urb2305", "xsb2305", "xsb2401", "uas0304", "ues0304", "urs0304", "xab2307", "uab2302", "ueb2401", "url0401", "xss0302", "urb2302", "ueb2302", "xsb2302", },
+        categories = { "uas0304", "ues0304", "urs0304", "uab2302", "xss0302", "urb2302", "ueb2302", "xsb2302", },
         name = "<LOC restricted_units_data_0012>No Game Enders",
         tooltip = "restricted_units_gameenders",
     },
 	
-    BUBBLES = {
+    SHIELDS = {
         categories = {"uel0307", "ual0307", "xsl0307", "xes0205", "ueb4202", "urb4202", "uab4202", "xsb4202", "ueb4301", "uab4301", "xsb4301", },
-        name = "<LOC restricted_units_data_0013>No Bubbles",
+        name = "<LOC restricted_units_data_0013>No Shield Units",
         tooltip = "restricted_units_bubbles",
     },
 	
@@ -169,6 +157,8 @@ GPGsortOrder = {
     "AIREXPERIMENTALS",	
 	"TACTICALMISSILELAUNCHERS",
     "NUKE",
+	"HEAVYARTILLERY",
+	"EXPERIMENTALARTILLERY",
     "GAMEENDERS",
     "BUBBLES",
     "INTEL",
