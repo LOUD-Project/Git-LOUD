7ef3218882162caa99947dc43ba736537120b93e
Axle <elitesouthpaw@gmail.com>
2018-04-26 17:50:40 -0500

    Fixed regular Anussons map markers

--- maps/anussons playground/Anussons_scenario.lua
+++ maps/anussons playground/Anussons_scenario.lua
@@ -3,7 +3,7 @@ ScenarioInfo = {
     name = "Anussons",
     description = "<LOC Anussons Playground_Description>The Anusson Islands group is a rich archipelago often hotly contested.  This is a moderately difficult AI map.",
     preview = '',
-    map_version = '7.6',
+    map_version = 7.7,
     type = 'skirmish',
     starts = true,
     size = {2048, 2048},
@@ -16,10 +16,11 @@ ScenarioInfo = {
             teams = {
                 {
                     name = 'FFA',
-                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6', 'ARMY_7', 'ARMY_8', 'ARMY_9', 'ARMY_10', 'ARMY_11', 'ARMY_12', 'ARMY_13', 'NEUTRAL_CIVILIAN'}
+                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6', 'ARMY_7', 'ARMY_8', 'ARMY_9', 'ARMY_10', 'ARMY_11', 'ARMY_12'}
                 },
             },
             customprops = {
+                ['ExtraArmies'] = STRING( 'NEUTRAL_CIVILIAN' )
             },
         },
     },
