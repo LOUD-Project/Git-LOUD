734d7ae46f2917d2bb2321243ae895f27e8b3c7a
Sprouto <loudman@rogers.com>
2018-07-05 17:48:53 -0400

    BrewLAN - Solaris script updates

--- gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SAA0306/SAA0306_Script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SAA0306/SAA0306_Script.lua
@@ -1,15 +1,10 @@
-#****************************************************************************
-#**
+#********************************************
 #**  Author(s):  Sean Wheeldon
-#**
-#**  Summary  :  Aeon T3 Transport Script
-#**
-#****************************************************************************
+#**  Summary  :  Aeon Transport Script
+#********************************************
 
 local AAirUnit = import('/lua/aeonunits.lua').AAirUnit
-local aWeapons = import('/lua/aeonweapons.lua')
-local util = import('/lua/utilities.lua')
-local AAATemporalFizzWeapon = aWeapons.AAATemporalFizzWeapon
+local AAATemporalFizzWeapon = import('/lua/aeonweapons.lua').AAATemporalFizzWeapon
 local explosion = import('/lua/defaultexplosions.lua')
 
 SAA0306 = Class(AAirUnit) {
@@ -18,12 +13,14 @@ SAA0306 = Class(AAirUnit) {
 
     ShieldEffects = { '/effects/emitters/aeon_shield_generator_t3_03_emit.bp' },
 
-    AirDestructionEffectBones = { 'Outer1', 'Outer002', 'Outer003', 'Outer004', 'Outer005', 'Outer006',
-				  'Outer007', 'Outer008', 'Outer009', 'Outer010', 'Outer011', 'Outer012',
-				  'Torus001', 'Torus002', 'Torus003', 'Torus004', 'Torus005', 'Torus006',
-				  'Torus007', 'Torus008', 'Torus009', 'Torus010', 'Torus011', 'Torus012',
-                  'Sphere', 'Shutter1', 'Shutter2', 'Shutter3', 'Shutter4', 'Shutter5', 'Shutter6',
-				  'Disk1', 'Disk2', },
+    AirDestructionEffectBones = {
+		'Outer1', 'Outer002', 'Outer003', 'Outer004', 'Outer005', 'Outer006',
+		'Outer007', 'Outer008', 'Outer009', 'Outer010', 'Outer011', 'Outer012',
+		'Torus001', 'Torus002', 'Torus003', 'Torus004', 'Torus005', 'Torus006',
+		'Torus007', 'Torus008', 'Torus009', 'Torus010', 'Torus011', 'Torus012',
+        'Sphere', 'Shutter1', 'Shutter2', 'Shutter3', 'Shutter4', 'Shutter5', 'Shutter6',
+		'Disk1', 'Disk2'
+	},
 
     Weapons = {
         AAFizz = Class(AAATemporalFizzWeapon) {},
@@ -32,9 +29,7 @@ SAA0306 = Class(AAirUnit) {
     OnStopBeingBuilt = function(self,builder,layer)
 	
         AAirUnit.OnStopBeingBuilt(self,builder,layer)
-		
-		self.ShieldEffectsBag = {}
-		
+
         self:HideBone('Shutter1', true)
         self:HideBone('Shutter2', true)
         self:HideBone('Shutter3', true)
@@ -47,70 +42,59 @@ SAA0306 = Class(AAirUnit) {
     OnShieldEnabled = function(self)
 	
         AAirUnit.OnShieldEnabled(self)
-
-        self.OrbManip1 = CreateRotator(self, 'Sphere', 'x', nil, 0, 45, -45)
-        self.Trash:Add(self.OrbManip1)
-
-        self.OrbManip1:SetTargetSpeed(-45)
-
-        self.OrbManip2 = CreateRotator(self, 'Sphere', 'z', nil, 0, 45, 45)
-        self.Trash:Add(self.OrbManip2)
-
-        self.OrbManip2:SetTargetSpeed(45)
-
-        self.DiskManip1 = CreateRotator(self, 'Disk1', 'x', nil, 0, 45, 45)
-        self.Trash:Add(self.DiskManip1)
-
-        self.DiskManip1:SetTargetSpeed(45)
-
-        self.DiskManip2 = CreateRotator(self, 'Disk2', 'y', nil, 0, 90, 90)
-        self.Trash:Add(self.DiskManip2)
-
-        self.DiskManip2:SetTargetSpeed(90)
-
-        if self.ShieldEffectsBag then
 		
-            for k, v in self.ShieldEffectsBag do
-                v:Destroy()
-            end
+        if not self.StressMeter then
+		
+            self.StressMeter = 45
 			
-			self.ShieldEffectsBag = {}
+        else
 		
-		end
-		
-        for k, v in self.ShieldEffects do
-		
-            table.insert( self.ShieldEffectsBag, CreateAttachedEmitter( self, 0, self:GetArmy(), v ):ScaleEmitter(1):OffsetEmitter(0,-3,0) )
+            self.StressMeter = math.min(45*5, self.StressMeter + 45)
 			
         end
+		
+        if not self.Manips then
+		
+            self.Manips = {}
+			
+            for i, v in {{'Sphere', 'x'}, {'Sphere', 'z'}, {'Disk1', 'x'}, {'Disk2', 'y'}} do
+                dir = 1 - 2 * math.random(0,1)
+                self.Manips[i] = CreateRotator(self, v[1], v[2], nil, 0, self.StressMeter, self.StressMeter * dir * math.max(1, i - 2))
+                self.Trash:Add(self.Manips[i])                                -- The math max makes the last rotator go double speed.
+            end
+			
+        else
+		
+            for i, v in self.Manips do
+                dir = 1 - 2 * math.random(0,1)
+                v:SetSpinDown(false)
+                v:SetTargetSpeed(self.StressMeter * dir * math.max(1, i - 2))
+            end
+			
+        end
+		
+        if not self.ShieldEffectsBag[1] then
+		
+            self.ShieldEffectsBag = {CreateAttachedEmitter( self, 0, self:GetArmy(), self.ShieldEffect ):ScaleEmitter(1):OffsetEmitter(0,-3,0)}
+			
+    	end
 		
     end,
 
     OnShieldDisabled = function(self)
 	
         AAirUnit.OnShieldDisabled(self)
-
-        self.OrbManip1:SetSpinDown(true)
-        self.OrbManip1:SetTargetSpeed(0)
-
-        self.OrbManip2:SetSpinDown(true)
-        self.OrbManip2:SetTargetSpeed(0)
 		
-        self.DiskManip1:SetSpinDown(true)
-        self.DiskManip1:SetTargetSpeed(0)
-
-        self.DiskManip2:SetSpinDown(true)
-        self.DiskManip2:SetTargetSpeed(0)
-
-        if self.ShieldEffectsBag then
+        for i, v in self.Manips do
+            v:SetSpinDown(true)
+            v:SetTargetSpeed(0)
+        end
 		
-            for k, v in self.ShieldEffectsBag do
-                v:Destroy()
-            end
-			
-			self.ShieldEffectsBag = {}
+        if self.ShieldEffectsBag[1] then
+            self.ShieldEffectsBag[1]:Destroy()
+    	end
 		
-		end
+        self.ShieldEffectsBag = nil
 		
     end,
 
@@ -122,47 +106,26 @@ SAA0306 = Class(AAirUnit) {
     end,
 
     OnKilled = function(self, instigator, type, overkillRatio)
+	
         AAirUnit.OnKilled(self, instigator, type, overkillRatio)
-
-        self.detector = CreateCollisionDetector(self)
-        self.Trash:Add(self.detector)
-        self.detector:WatchBone('Attachpoint02')
-        self.detector:WatchBone('Attachpoint008')
-        self.detector:WatchBone('Attachpoint014')
-        self.detector:WatchBone('Attachpoint020')
-        self.detector:WatchBone('Attachpoint026')
-        self.detector:WatchBone('Attachpoint032')
-        self.detector:WatchBone('Attachpoint038')
-        self.detector:WatchBone('Attachpoint044')
-        self.detector:WatchBone('Attachpoint050')
-        self.detector:WatchBone('Attachpoint056')
-        self.detector:WatchBone('Attachpoint062')
-        self.detector:WatchBone('Attachpoint068')
-        self.detector:WatchBone('Attachpoint01')
-        self.detector:WatchBone('Attachpoint012')
-        self.detector:WatchBone('Attachpoint018')
-        self.detector:WatchBone('Attachpoint024')
-        self.detector:WatchBone('Attachpoint030')
-        self.detector:WatchBone('Attachpoint036')
-        self.detector:WatchBone('Attachpoint042')
-        self.detector:WatchBone('Attachpoint058')
-        self.detector:WatchBone('Attachpoint054')
-        self.detector:WatchBone('Attachpoint060')
-        self.detector:WatchBone('Attachpoint066')
-        self.detector:WatchBone('Attachpoint072')
-        self.detector:EnableTerrainCheck(true)
-        self.detector:Enable()
-
-        #self.OrbManip1:Destroy()
-        #self.OrbManip1 = nil
-        #self.OrbManip2:Destroy()
-        #self.OrbManip2 = nil
-        #self.DiskManip1:Destroy()
-        #self.DiskManip1 = nil
-        #self.DiskManip2:Destroy()
-        #self.DiskManip2 = nil
-
-        self:TransportDetachAllUnits(true)
+		
+        if self:GetCurrentLayer() == 'Air' then
+		
+            self.detector = CreateCollisionDetector(self)
+			
+            self.Trash:Add(self.detector)
+			
+            --Attachpoint01-6 have 2 digits
+            --Attachpoint007-72 have 3 digits.
+            for i = 1, 14 do
+                self.detector:WatchBone('Attachpoint0' .. i * 5)
+            end
+			
+            self.detector:EnableTerrainCheck(true)
+            self.detector:Enable()
+            self:TransportDetachAllUnits(true)
+			
+        end
 		
     end,
 
