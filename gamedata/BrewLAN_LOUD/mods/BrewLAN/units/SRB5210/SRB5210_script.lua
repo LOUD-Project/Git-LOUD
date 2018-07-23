904c9608b2ff6c61c61bdc56dd366e95eec9dbe7
Sprouto <loudman@rogers.com>
2018-07-23 16:38:42 -0400

    BrewLAN - Seraphim T2 wall segments
    Updated to Medium in description, costed in relation to existing low T2 wall segments and .5 unit cap cost.

--- gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SRB5210/SRB5210_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SRB5210/SRB5210_script.lua
@@ -1,10 +1,7 @@
-#****************************************************************************
-#** 
-#**  Cybran Wall: With cordinal scripting
-#** 
-#****************************************************************************
 local CWallStructureUnit = import('/lua/cybranunits.lua').CWallStructureUnit
+
 local CardinalWallUnit = import(import( '/lua/game.lua' ).BrewLANPath() .. '/lua/walls.lua').CardinalWallUnit
+
 CWallStructureUnit = CardinalWallUnit( CWallStructureUnit )
 
 SRB5210 = Class(CWallStructureUnit) {}
--- gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SSB5210/SSB5210_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SSB5210/SSB5210_script.lua
@@ -1,10 +1,7 @@
-#****************************************************************************
-#** 
-#**  Cybran Wall: With cordinal scripting
-#** 
-#****************************************************************************
 local SWallStructureUnit = import('/lua/seraphimunits.lua').SWallStructureUnit
+
 local CardinalWallUnit = import(import( '/lua/game.lua' ).BrewLANPath() .. '/lua/walls.lua').CardinalWallUnit
+
 SWallStructureUnit = CardinalWallUnit( SWallStructureUnit )
 
 SSB5210 = Class(SWallStructureUnit) {}
