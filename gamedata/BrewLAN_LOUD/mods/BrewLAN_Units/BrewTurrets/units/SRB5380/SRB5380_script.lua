44cc1dfef86e34a1ac4c921a619496bb9f1e6854
Sprouto <loudman@rogers.com>
2018-08-07 22:09:23 -0400

    BrewLAN - final unit cuts and temporarily removed additional unit mods

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_Units/BrewTurrets/units/SRB5380/SRB5380_script.lua
+++
@@ -1,5 +0,0 @@
-local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
-
-SRB5380 = Class(CStructureUnit) {}
-
-TypeClass = SRB5380
--- gamedata/BrewLAN_LOUD/mods/BrewLAN_Units/BrewTurrets/units/SSB2380/SSB2380_Script.lua
+++
@@ -1,9 +0,0 @@
-local SStructureUnit = import('/lua/seraphimunits.lua').SStructureUnit
-local DisarmBeamWeapon = import('/mods/brewlan_units/brewturrets/lua/weapons.lua').DisarmBeamWeapon
-
-SSB2380 = Class(SStructureUnit) {
-    Weapons = {
-        MainGun = Class(DisarmBeamWeapon) {},
-    },
-}
-TypeClass = SSB2380
