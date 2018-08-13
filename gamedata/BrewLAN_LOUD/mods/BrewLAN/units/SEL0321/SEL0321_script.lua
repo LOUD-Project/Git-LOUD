695c750f3f786ac03b7e4b503becbb7b7d9f48d3
Sprouto <loudman@rogers.com>
2018-08-13 14:51:07 -0400

    BrewLAN - corrections
    UEF mobile anti-nuke, Cybran Mobile AA

--- gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SEL0321/SEL0321_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN/units/SEL0321/SEL0321_script.lua
@@ -9,11 +9,11 @@ SEL0321 = Class(TLandUnit) {
         AntiNuke = Class(TAMInterceptorWeapon) {
             RackSalvoFireReadyState = State(TAMInterceptorWeapon.RackSalvoFireReadyState) {
                 Main = function(self)
-                    if self.unit:GetTacticalSiloAmmoCount() < 3 then
+                    if self.unit:GetTacticalSiloAmmoCount() < 2 then
                         self:ForkThread(
                             function(self)
                                 WaitTicks(1)
-                                if self.unit:GetTacticalSiloAmmoCount() > 2 then
+                                if self.unit:GetTacticalSiloAmmoCount() > 1 then
                                     --Last minute panic check, not sure if it will actually work, very hard to test chance to test it
                                     TAMInterceptorWeapon.RackSalvoFireReadyState.Main(self)
                                 end
