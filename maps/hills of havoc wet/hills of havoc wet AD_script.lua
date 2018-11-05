39fdfe589fc6d0d1896bee804489fd803cdf9b00
Sprouto <loudman@rogers.com>
2018-11-04 21:19:03 -0500

    MAPS - Hills of Havoc Wet - AD cleanup
    A better layout of ALL the start locations to minimize resource shifts at start of game and make all resources equidistant

--- maps/hills of havoc wet/hills of havoc wet AD_script.lua
+++
@@ -1,9 +0,0 @@
-local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
-local ScenarioFramework = import('/lua/ScenarioFramework.lua')
-
-function OnPopulate()
-  ScenarioUtils.InitializeArmies()
-end
-
-function OnStart(self)
-end
