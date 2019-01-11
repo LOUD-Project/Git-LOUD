f3617f95ffa44ce76741a83e87c424f6ca4e0b28
Sprouto <loudman@rogers.com>
2019-01-11 13:07:47 -0500

    BO Unleashed - Removed Goliath Mk. 1

--- gamedata/BlackOps/mods/BlackOpsUnleashed/lua/CustomUnits/BO_LandUnits.lua
+++ gamedata/BlackOps/mods/BlackOpsUnleashed/lua/CustomUnits/BO_LandUnits.lua
@@ -1,58 +1,57 @@
 UnitList = {
 	T1LandDFTank = {
-		Seraphim = {'bsl0106', 25}, #Protector Bot
+		Seraphim = {'bsl0106', 25}, 		# Protector Bot
 	},
 
 	T2AttackTank = {
-		UEF = {'bel0211', 30 },     	# Flamethrower
-        Aeon = {'bal0206', 50 },    	# Zealot
-        Cybran = {'brl0205', 15 },  	# Ambush Bot
-		Seraphim = {'bsl0206', 25 },	# Imp. Protector Bot
+		UEF = {'bel0211', 30 },     		# Flamethrower
+        Aeon = {'bal0206', 50 },    		# Zealot
+        Cybran = {'brl0205', 15 },  		# Ambush Bot
+		Seraphim = {'bsl0206', 25 },		# Imp. Protector Bot
 	},
     
     T3LandAA = {
-        UEF = { 'belk002', 100 },		# Cougar
-        Aeon = { 'balk003', 100 },		# Redeemer
-        Cybran = { 'brlk001', 100 },	# Bouncer
-        Seraphim = { 'bslk004', 100 },	# Lightning Tank
+        UEF = { 'belk002', 100 },			# Cougar
+        Aeon = { 'balk003', 100 },			# Redeemer
+        Cybran = { 'brlk001', 100 },		# Bouncer
+        Seraphim = { 'bslk004', 100 },		# Lightning Tank
     },
     
     T3LandArtillery = {
-        UEF = { 'bel0308', 20 },    	# bangerhead arty support bot 
+        UEF = { 'bel0308', 20 },    		# bangerhead arty support bot 
     },
     
     T3LandArtilleryElite = {
-        Seraphim = {'bsl0007', 100}, 	# Elite T3 Artillery
+        Seraphim = {'bsl0007', 100}, 		# Elite T3 Artillery
     },
     
     T3LandBotElite = {
-        Seraphim = {'bsl0005', 100}, 	# Elite Siege Tank
+        Seraphim = {'bsl0005', 100}, 		# Elite Siege Tank
     },
     
     T3ArmoredAssault = {
-    	UEF = {'xel0307', 100 },     	# Juggernaut
-    	Aeon = {'bal0310', 100 },	   	# Wraith Hovertank
+    	UEF = {'xel0307', 100 },     		# Juggernaut
+    	Aeon = {'bal0310', 100 },	   		# Wraith Hovertank
     },
 	
 	T3Amphibious = {
-    	UEF = {'xel0307', 50 },     	# Juggernaut
-    	Aeon = {'bal0310', 100 },	   	# Wraith Hovertank
+    	UEF = {'xel0307', 50 },     		# Juggernaut
+    	Aeon = {'bal0310', 100 },	   		# Wraith Hovertank
 	},
     
     T3LandBot = {
 
-    	Cybran = {'brl0307', 25 },  	# Hailfire
-    	Seraphim = {'bsl0310', 100},    # Lambda Bot        
+    	Cybran = {'brl0307', 25 },  		# Hailfire
+    	Seraphim = {'bsl0310', 100},    	# Lambda Bot        
     },
 	
 	T4LandExperimental1 = {
-		Aeon = {'bal0402', 50},			# Genesis Hover Exp	
-		Seraphim = {'bsl0406', 100},	# Exp Protector	
+		Aeon = {'bal0402', 50},				# Genesis Hover Exp	
+		Seraphim = {'bsl0406', 100},		# Exp Protector	
 	},
    
 	T4LandExperimental2 = {
-		UEF = {'xel0401', 45}, 			# Goliath (original Goliath)
-		Aeon = {'bal0403', 20},			# Interrogator (old Inquisitor)		
+		Aeon = {'bal0403', 20},				# Interrogator (old Inquisitor)		
 	},
 	
 	T4LandExperimental3 = {
