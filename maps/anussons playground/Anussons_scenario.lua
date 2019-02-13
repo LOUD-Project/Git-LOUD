c6ba09dc04a1e1c76f81cfc59e45f06da9d92382
Sprouto <loudman@rogers.com>
2019-02-13 14:54:52 -0500

    MAPS - updated markers, minor edits
    Annusons, Lost Temple, The Pass 4 v 1

--- maps/anussons playground/Anussons_scenario.lua
+++ maps/anussons playground/Anussons_scenario.lua
@@ -1,19 +1,16 @@
 version = 3 -- Lua Version. Dont touch this
-
 ScenarioInfo = {
     name = "Anussons",
     description = "<LOC Anussons Playground_Description>The Anusson Islands group is a rich archipelago often hotly contested.  This is a moderately difficult AI map.",
     preview = '',
-    map_version = '7.9',
+    map_version = '8',
     type = 'skirmish',
     starts = true,
     size = {2048, 2048},
     map = '/maps/anussons playground/Anussons.scmap',
     save = '/maps/anussons playground/Anussons_save.lua',
     script = '/maps/anussons playground/Anussons_script.lua',
-	
     norushradius = 75,
-	
     Configurations = {
         ['standard'] = {
             teams = {
