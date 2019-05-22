fff975d99fe518a3dea75b653d7c53ce58c75b94
Balthazar <the.balthazar@Gmail.com>
2019-05-22 18:56:32 +0100

    buildmodedata for BlOps ACUs

---
+++ gamedata/BlackOps/mods/BlackopsACUs/hook/lua/ui/game/buildmodedata.lua
@@ -0,0 +1,24 @@
+buildModeKeys['erl0001'] = {
+    [1] = CybranT1Eng,
+    [2] = CybranT2Eng,
+    [3] = CybranT3Eng,
+    [4] = CybranT4Eng
+}
+buildModeKeys['eel0001'] = {
+    [1] = UEFT1Eng,
+    [2] = UEFT2Eng,
+    [3] = UEFT3Eng,
+    [4] = UEFT4Eng
+}
+buildModeKeys['eal0001'] = {
+    [1] = AeonT1Eng,
+    [2] = AeonT2Eng,
+    [3] = AeonT3Eng,
+    [4] = AeonT4Eng
+}
+buildModeKeys['esl0001'] = {
+    [1] = SeraphimT1Eng,
+    [2] = SeraphimT2Eng,
+    [3] = SeraphimT3Eng,
+    [4] = SeraphimT4Eng
+}
