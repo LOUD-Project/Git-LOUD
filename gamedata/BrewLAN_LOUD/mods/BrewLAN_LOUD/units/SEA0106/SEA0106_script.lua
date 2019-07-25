070119395a7702612997848d1fa78bb51f9e2f15
Sprouto <loudman@rogers.com>
2019-07-25 15:37:51 -0400

    BrewLAN - add T1 torpedo bombers

---
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SEA0106/SEA0106_script.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+--  Summary  :  UEF Torpedo Bomber Script
+--------------------------------------------------------------------------------
+local TAirUnit = import('/lua/terranunits.lua').TAirUnit
+local TANTorpedoAngler = import('/lua/terranweapons.lua').TANTorpedoAngler
+
+SEA0106 = Class(TAirUnit) {
+    Weapons = {
+        Torpedo = Class(TANTorpedoAngler) {},
+    },
+}
+
+TypeClass = SEA0106
