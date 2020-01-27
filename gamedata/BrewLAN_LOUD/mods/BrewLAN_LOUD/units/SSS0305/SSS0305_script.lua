9df00b8bef395e90dca0b203e87fe1cffc861583
Sprouto1 <loudman@rogers.com>
2020-01-26 20:28:13 -0500

    BrewLAN - Sera T3 Sonar
    Balanced on the radar/sonar formula
    Sera T3 Torpedo Bomber - added category class

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SSS0305/SSS0305_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SSS0305/SSS0305_script.lua
@@ -1,13 +1,3 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB3202/UAB3202_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
-#**
-#**  Summary  :  Aeon Long Range Sonar Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
-
 local SSonarUnit = import('/lua/seraphimunits.lua').SSonarUnit
 
 SSS0305 = Class(SSonarUnit) {
