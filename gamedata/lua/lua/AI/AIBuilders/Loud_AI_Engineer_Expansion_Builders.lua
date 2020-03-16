dbeaed080b3ca3195d605bfde24313dfefd92c0e
Sprouto1 <loudman@rogers.com>
2020-03-16 18:21:23 -0400

    AI - Tuning Naval Base creation
    Naval bases should never be created if there is an economy threat with 12km of the base - to maximize air/ground efforts.

--- gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Expansion_Builders.lua
+++ gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Expansion_Builders.lua
@@ -317,8 +317,8 @@ BuilderGroup {BuilderGroupName = 'Engineer Naval Expansion Construction',
 			-- must have 3+ factories at MAIN
             { UCBC, 'UnitsGreaterAtLocation', { 'LocationType', 3, categories.FACTORY * categories.STRUCTURE}},
 			
-			-- can't be a major enemy base with 10km of here
-			{ TBC, 'ThreatFurtherThan', { 'LocationType', 500, 'Economy', 300 }},
+			-- can't be a major enemy base with 12.5km of here
+			{ TBC, 'ThreatFurtherThan', { 'LocationType', 650, 'Economy', 150 }},
 			
 			-- find a safe, unused, naval marker within 12km of this base
             { UCBC, 'NavalAreaForExpansion', { 'LocationType', 600, -250, 50, 2, 'AntiSurface' } },
