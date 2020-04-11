ec5b2013823590bb3b16a9d09f241a609f85defd
Sprouto1 <loudman@rogers.com>
2020-04-11 13:25:19 -0400

    LUA - Vers 5.2.04
    Featuring a single AI multiplier and the elimination of the AIx AI - in favor of a single AI choice.

--- gamedata/lua/lua/AI/CustomAIs_v2/ExtrasAI.lua
+++ gamedata/lua/lua/AI/CustomAIs_v2/ExtrasAI.lua
@@ -1,18 +1,21 @@
 AI = {
 	Name = "AI Patch LOUD",
 	
-	Version = "5.2.03",
-	
+	Version = "5.2.04",
+
 	AIList = {
 		{
 			key = 'loud',
 			name = "AI: LOUD",
 		},
 	},
+
+--[[
 	CheatAIList = {
 		{
 			key = 'loudcheat',
 			name = "AIx: LOUDER",
 		},
 	},
+--]]
 }
