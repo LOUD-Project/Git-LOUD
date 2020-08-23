537cc4a490d56cbb9256d7914af2cba7451af6a8
rajderks <2419711+rajderks@users.noreply.github.com>
2020-08-23 22:46:31 +0200

    pruned Aeon Nova Cat proj

--- gamedata/TotalMayhem/mods/TotalMayhem/lua/TMprojectiles.lua
+++ gamedata/TotalMayhem/mods/TotalMayhem/lua/TMprojectiles.lua
@@ -65,25 +65,6 @@ AeonT3SHPDproj = Class(MultiPolyTrailProjectile) {
 }
 
 #----------------
-# Aeon Tech 3 NovaCat Quantum Charge
-#----------------
-AeonBROT3NCMproj = Class(SinglePolyTrailProjectile) {
-    FxTrails = {
-        '/effects/emitters/quantum_cannon_munition_03_emit.bp',
-        '/effects/emitters/quantum_cannon_munition_04_emit.bp',
-    },
-    PolyTrail = '/effects/emitters/quantum_cannon_polytrail_01_emit.bp',
-    FxImpactUnit = TMEffectTemplate.AeonQuantumChargeHit01,
-    FxUnitHitScale = 1.2,
-    FxImpactProp = TMEffectTemplate.AeonQuantumChargeHit01,
-    FxPropHitScale = 1.2,
-    FxImpactLand = TMEffectTemplate.AeonQuantumChargeHit01,
-    FxLandHitScale = 1.2,
-    FxTrailOffset = 0,
-    FxImpactUnderWater = {},
-}
-
-#----------------
 # Aeon Tech 3 Rocket Defense
 #----------------
 AeonBROT3PDROproj = Class(MultiPolyTrailProjectile) {
