808ef7b32825df51356778f0af3fcdce47d043db
Sprouto1 <loudman@rogers.com>
2020-08-28 13:33:54 -0400

    LOUD MODS - Unit Additions v5.4
    Removed rebuild bonuses for T4AA and Barrage Artillery.
    Removed Fatboy II from release.

--- gamedata/LOUD_Mods/mods/LOUD Unit Additions/mod_info.lua
+++ gamedata/LOUD_Mods/mods/LOUD Unit Additions/mod_info.lua
@@ -1,7 +1,7 @@
 name = "LOUD Unit Additions"
 uid = "fffffffe-6e98-4864-9599-4133236eea7a"
 
-version = '5.3'
+version = '5.4'
 
 copyright = "None"
 description = "Integrates additional experimentals, shields, defenses & utility buildings.  All are fully LOUD balanced and AI functional."
