a708e19714cbba386b077e052d3e92cf700191b4
Sprouto1 <loudman@rogers.com>
2020-09-29 19:55:18 -0400

    LUA - Platoon Formation Manager cycle time
    The PFM is responsible for forming platoons out of units at a base.   It runs on a cycle that is based upon the length of time the AI requires to process ALL the static build conditions.  
    Previously the PFM ran at HALF that speed, resulting in conditions where it might be 2.5 minutes between formation of new platoons.    This was too long.   
    The PRIMARY base will now run at full speed, with an adjustment for the AIMultiplier.  The results in dramatically less time between formation creations - allowing the PRIMARY to make far more immediate use of the resources at hand.

--- gamedata/lua/lua/AI/aiutilities.lua
+++ gamedata/lua/lua/AI/aiutilities.lua
@@ -987,6 +987,7 @@ function SetupAICheatUnitCap(aiBrain, biggestTeamSize)
         local cheatCap = initialCap * tonumber(ScenarioInfo.Options.AIMult or 1) * (math.max(PlayerDiff,1))
 
         -- Veterancy mult is always 1 or higher
+        -- more important - it's also represents the AIMultiplier for this brain
 		aiBrain.VeterancyMult = math.max( 1, tonumber(ScenarioInfo.Options.AIMult or 1))
 
         SetArmyUnitCap( aiBrain.ArmyIndex, math.floor(cheatCap) )
