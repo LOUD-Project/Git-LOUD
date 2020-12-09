8a385a9bfe5bf4a77081dda827a07e3ede5a6876
Sprouto1 <loudman@rogers.com>
2020-12-09 14:19:12 -0500

    LUA - AI Naval Base creation
    Naval Bases were suppressed if a major enemy threat was within 13km.   On some maps, this is pretty much guaranteed - such as Lily Pads.   This feature was originally intended to keep an AI out of the water if the enemy was close by.  
    
    In that respect - it is valid (it was created to help with Seton's Clutch situations) - but it's to broad.  Reduced the range to 12km (600) and increased the threat trigger to 1500.   This should permit artificial threat (at game start) to decline enough, at a soon enough point, to allow naval bases to form.

--- gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Expansion_Builders.lua
+++ gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Expansion_Builders.lua
@@ -431,8 +431,8 @@ BuilderGroup {BuilderGroupName = 'Engineer Naval Expansion Construction',
 			-- must have 3+ factories at MAIN
             { UCBC, 'UnitsGreaterAtLocation', { 'LocationType', 3, categories.FACTORY * categories.STRUCTURE}},
 			
-			-- can't be a major enemy base with 12.5km of here
-			{ TBC, 'ThreatFurtherThan', { 'LocationType', 650, 'Economy', 250 }},
+			-- can't be a major enemy base within 12km of here
+			{ TBC, 'ThreatFurtherThan', { 'LocationType', 600, 'Economy', 1500 }},
 			
 			-- find a safe, unused, naval marker within 12km of this base
             { UCBC, 'NavalAreaForExpansion', { 'LocationType', 600, -250, 50, 2, 'AntiSurface' } },
