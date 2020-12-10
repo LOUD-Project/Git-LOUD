ddad04a6d4866da171e53f3a369bc3a87488bb8e
Unknown <tanks.co@outlook.com>
2020-12-10 00:51:30 +0000

    Brought back some shit colors by popular demand

--- gamedata/lua/lua/GameColors.lua
+++ gamedata/lua/lua/GameColors.lua
@@ -1,50 +1,86 @@
 GameColors = {
     PlayerColors = {
-	"436eee",
-    "e80a0a",
-    "616d7e",
-    "fafa00",
-    "FF873E",
-    "ffffff",
-    "9161ff",
-    "ff88ff",
-    "2e8b57",
-    "2929e1",
-    "5F01A7",
-    "ff32ff",
-    "a79602",
-    "b76518",
-    "901427",
-    "2F4F4F",
-    "40bf40",
-    "66ffcc",
-    "9fd802",
-	"FFFFFF",
-	"000000"
+	"e80a0a",		-- Red
+	"991427",		-- Mid Red
+	"4C0000",		-- Dark Red
+	
+	"FF873E",		-- Orange
+	"b76518",		-- Dark Orange/Light Brown
+	"4C3100",		-- Brown
+	
+	"fafa00",		-- Yellow
+	"a79602",		-- Mid Yellow/Tarnished Gold
+	"4C4C00",		-- Dark Yellow
+	
+	"39FF14",		-- Curste Green
+	"40bf40",		-- Mid Green
+	"004C00",		-- Dark Green
+
+	"00FFFF",		-- Aqua
+	"009999",		-- Mid Aqua
+	"2F4F4F",		-- Dark Aqua
+	
+	"436eee",       -- Blue
+	"2929e1",		-- Mid Blue
+	"00004C",		-- Dark Blue
+
+	"ff32ff",		-- Fuschia
+	"990099",		-- Magenta
+	"4C004C",		-- Dark Pink
+	
+	"ff88ff",		-- Light Pink
+	"9161ff",		-- Purple
+	"5F01A7",		-- Dark Purple
+
+	"FFFFFF",		-- White
+	
+	"616d7e",		-- Gray
+	"40404A",		-- Dark Gray
+	"25252B",		-- Very Dark Gray
+	
+	"000000"		-- Black
     },
 
     ArmyColors = {
-	"436eee",
-    "e80a0a",
-    "616d7e",
-    "fafa00",
-    "FF873E",
-    "ffffff",
-    "9161ff",
-    "ff88ff",
-    "2e8b57",
-    "2929e1",
-    "5F01A7",
-    "ff32ff",
-    "a79602",
-    "b76518",
-    "901427",
-    "2F4F4F",
-    "40bf40",
-    "66ffcc",
-    "9fd802",
-	"FFFFFF",
-	"000000"
+	"e80a0a",		-- Red
+	"991427",		-- Mid Red
+	"4C0000",		-- Dark Red
+	
+	"FF873E",		-- Orange
+	"b76518",		-- Dark Orange/Light Brown
+	"4C3100",		-- Brown
+	
+	"fafa00",		-- Yellow
+	"a79602",		-- Mid Yellow/Tarnished Gold
+	"4C4C00",		-- Dark Yellow
+	
+	"39FF14",		-- Curste Green
+	"40bf40",		-- Mid Green
+	"004C00",		-- Dark Green
+
+	"00FFFF",		-- Aqua
+	"009999",		-- Mid Aqua
+	"2F4F4F",		-- Dark Aqua
+	
+	"436eee",       -- Blue
+	"2929e1",		-- Mid Blue
+	"00004C",		-- Dark Blue
+
+	"ff32ff",		-- Fuschia
+	"990099",		-- Magenta
+	"4C004C",		-- Dark Pink
+	
+	"ff88ff",		-- Light Pink
+	"9161ff",		-- Purple
+	"5F01A7",		-- Dark Purple
+
+	"FFFFFF",		-- White
+	
+	"616d7e",		-- Gray
+	"40404A",		-- Dark Gray
+	"25252B",		-- Very Dark Gray
+	
+	"000000"		-- Black
     },
 
     CivilianArmyColor = "DEB887",	-- Burly Wood
@@ -52,8 +88,8 @@ GameColors = {
     TeamColorMode = {
         Self = "0000FF",		-- Blue
         Enemy = "FF0000",		-- Red
-        Ally = "009900",		-- Mid Green
-        Neutral = "999900",		-- Mid Yellow/Tarnished Gold
+        Ally = "39FF14",		-- Green
+        Neutral = "a79602",		-- Mid Yellow/Tarnished Gold
     },
 
     UnidentifiedColor = "808080",	-- Light Gray
