d704b42e9fb6d5888e22f98d1c39b8c053fca133
Unknown <tanks.co@outlook.com>
2020-12-10 00:02:36 +0000

    Updated Colors to match FAF colors (+/- a couple extra)

--- gamedata/lua/lua/GameColors.lua
+++ gamedata/lua/lua/GameColors.lua
@@ -1,72 +1,50 @@
 GameColors = {
     PlayerColors = {
-	"FF0000",		-- Red
-	"FFA500",		-- Orange
-	"FFFF00",		-- Yellow
-	"39FF14",		-- Curste Green
-	"20CE00",		-- Green
-	"00FFFF",		-- Aqua
-	"0000FF",		-- Blue
-	"FF00FF",		-- Magenta
-	"999999",		-- Gray
-	
-	"990000",		-- Mid Red
-	"996300",		-- Dark Orange/Light Brown
-	"999900",		-- Mid Yellow/Tarnished Gold
-	"009900",		-- Mid Green
-	"009999",		-- Mid Aqua
-	"000099",		-- Mid Blue
-	"990099",		-- Mid Magenta
-	"4C4C4C",		-- Dark Gray
-	
-	"4C0000",		-- Dark Red
-	"4C3100",		-- Brown
-	"4C4C00",		-- Dark Yellow
-	"004C00",		-- Dark Green
-	"004C4C",		-- Dark Aqua
-	"00004C",		-- Dark Blue
-	"4C004C",		-- Dark Magenta
-	"191919",		-- Very Dark Gray
-
-	"FFFFFF",		-- White	
-	"000000"		-- Black
+	"436eee",
+    "e80a0a",
+    "616d7e",
+    "fafa00",
+    "FF873E",
+    "ffffff",
+    "9161ff",
+    "ff88ff",
+    "2e8b57",
+    "2929e1",
+    "5F01A7",
+    "ff32ff",
+    "a79602",
+    "b76518",
+    "901427",
+    "2F4F4F",
+    "40bf40",
+    "66ffcc",
+    "9fd802",
+	"FFFFFF",
+	"000000"
     },
 
     ArmyColors = {
-	"FF0000",		-- Red
-	"990000",		-- Mid Red
-	"4C0000",		-- Dark Red
-	
-	"FFA500",		-- Orange
-	"996300",		-- Dark Orange/Light Brown
-	"4C3100",		-- Brown
-	
-	"FFFF00",		-- Yellow
-	"999900",		-- Mid Yellow/Tarnished Gold
-	"4C4C00",		-- Dark Yellow
-	
-	"39FF14",		-- Curste Green
-	"20CE00",		-- Green
-	"009900",		-- Mid Green
-	"004C00",		-- Dark Green
-
-	"00FFFF",		-- Aqua
-	"009999",		-- Mid Aqua
-	"004C4C",		-- Dark Aqua
-	
-	"0000FF",		-- Blue
-	"000099",		-- Mid Blue
-	"00004C",		-- Dark Blue
-
-	"FF00FF",		-- Magenta
-	"990099",		-- Mid Magenta
-	"4C004C",		-- Dark Magenta
-
-	"FFFFFF",		-- White
-	"999999",		-- Gray
-	"4C4C4C",		-- Dark Gray
-	"191919",		-- Very Dark Gray
-	"000000"		-- Black
+	"436eee",
+    "e80a0a",
+    "616d7e",
+    "fafa00",
+    "FF873E",
+    "ffffff",
+    "9161ff",
+    "ff88ff",
+    "2e8b57",
+    "2929e1",
+    "5F01A7",
+    "ff32ff",
+    "a79602",
+    "b76518",
+    "901427",
+    "2F4F4F",
+    "40bf40",
+    "66ffcc",
+    "9fd802",
+	"FFFFFF",
+	"000000"
     },
 
     CivilianArmyColor = "DEB887",	-- Burly Wood
