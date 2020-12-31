48a97cbda8954c2148216a1b6c28d3e6cc66dd1f
TheRatCircus <bloxbuilder@gmail.com>
2020-12-31 09:00:22 -0500

    LOUDMODS: New individualised icons for the LOUD minimods by Lohkrim
    Courtesy of Discord user Lohkrim#5559. Now these little mods are identifiable at a glance with some stylish graphics. The original icon has not been removed in case we need it for anything else.

--- gamedata/LOUD_Mods/mods/Artillery/mod_info.lua
+++ gamedata/LOUD_Mods/mods/Artillery/mod_info.lua
@@ -1,9 +1,10 @@
 name = "Enhanced T4 Artillery"
 author = "Henry Katzmarek"
 version = 1.04
-description = "Increases T4 artillery firepower"
+description = "Increases T4 artillery firepower."
 exclusive = false
 ui_only = false
+icon = "/mods/Artillery/Improved_Artillery.png"
 selectable = true
 uid = "ffffffff-9d4e-11dc-8314-0800200c0702"
 
