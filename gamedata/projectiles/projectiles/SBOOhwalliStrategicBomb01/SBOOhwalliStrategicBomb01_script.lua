e9c7a20708087fde0ca809e69276806266ae56b4
TheRatCircus <bloxbuilder@gmail.com>
2021-01-10 15:21:28 -0500

    PROJ: Prevent Ahwassa bomb creating FX or decal on shield hit

--- gamedata/projectiles/projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
+++ gamedata/projectiles/projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
@@ -2,8 +2,11 @@ local SOhwalliStrategicBombProjectile = import('/lua/seraphimprojectiles.lua').S
 
 SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile){
     OnImpact = function(self, TargetType, TargetEntity)
+        SOhwalliStrategicBombProjectile.OnImpact(self, TargetType, TargetEntity)
+        if TargetType == 'Shield' then
+            return
+        end
         self:CreateProjectile('/effects/entities/SBOOhwalliBombEffectController01/SBOOhwalliBombEffectController01_proj.bp', 0, 0, 0, 0, 0, 0):SetCollision(false)
-        SOhwalliStrategicBombProjectile.OnImpact(self, TargetType, TargetEntity) 
     end,
 }
 TypeClass = SBOOhwalliStategicBomb01
