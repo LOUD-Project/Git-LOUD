fc3f8665c42bd9ce47ae65969c92a6328067ca77
TheRatCircus <bloxbuilder@gmail.com>
2021-01-10 18:57:28 -0500

    LOUDMODS: Tidy up mod_info files

--- gamedata/LOUD_Mods/mods/Commanders/mod_info.lua
+++ gamedata/LOUD_Mods/mods/Commanders/mod_info.lua
@@ -6,6 +6,7 @@ exclusive = false
 ui_only = false
 selectable = true
 uid = "ffffffff-9d4e-11dc-8314-0800200c0605"
-requires = { "9e8ea941-c306-4751-b367-e00000000302",#BlackOps Adv Command Units
-	   }
+requires = {
+	"9e8ea941-c306-4751-b367-e00000000302", --BlackOps Adv Command Units
+}
 icon = "/mods/Commanders/Enhanced_Commanders.png"
