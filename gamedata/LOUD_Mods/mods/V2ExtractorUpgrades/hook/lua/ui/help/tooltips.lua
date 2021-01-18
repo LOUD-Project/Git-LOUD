b41b80efb09b36cb13cb4be7ad645580092f29fe
TheRatCircus <bloxbuilder@gmail.com>
2021-01-17 23:23:09 -0500

    LOUDMODS: Make Seq. Mex Upgrade tooltip accurate
    Thanks to MostLostNoob from the Discord.

--- gamedata/LOUD_Mods/mods/V2ExtractorUpgrades/hook/lua/ui/help/tooltips.lua
+++ gamedata/LOUD_Mods/mods/V2ExtractorUpgrades/hook/lua/ui/help/tooltips.lua
@@ -1,5 +1,5 @@
 Tooltips['seq_mex_upgrade'] = {
-	title = 'Upgrade Buildings Sequentially',
-	description = 'Upgrade selected mass extractors, shields, factories, and power gens one-at-a-time until all are max level.',
+	title = "Upgrade Buildings Sequentially",
+	description = "Upgrade selected mass extractors, shields, factories, and power gens one-at-a-time to each structure's next level.",
 	keyId = 'seq_mex_upgrade',
 }
