0082b939b40b0c5bc9062d3934d8946ced66b2ca
TheRatCircus <bloxbuilder@gmail.com>
2021-01-18 19:56:29 -0500

    BO, BL: Stop using "Dreadnought" in frontend unit naming
    Balthazar correctly identified inconsistencies in some experimental naval unit naming.

--- gamedata/BlackOps/mods/BlackOpsUnleashed/lua/CustomUnits/BO_SeaUnits.lua
+++ gamedata/BlackOps/mods/BlackOpsUnleashed/lua/CustomUnits/BO_SeaUnits.lua
@@ -1,25 +1,25 @@
 UnitList = {
 
 	T3Battlecruiser = {
-		Cybran = {'brs0304', 100},  	# Escort Cruiser
-		Seraphim = {'bss0306', 100}, 	# Heavy Cruiser
+		Cybran = {'brs0304', 100}, -- Escort Cruiser
+		Seraphim = {'bss0306', 100}, -- Heavy Cruiser
 	},
 	
     T2SeaCruiser = {
-        Seraphim = {'bss0206', 50}, 	# Bombardment Ship
+        Seraphim = {'bss0206', 50}, -- Bombardment Ship
     },
 	
 	T3SubKiller = {
-		Cybran = {'brs0305', 100}, 		# Assault Sub
+		Cybran = {'brs0305', 100}, -- Assault Sub
 	},
 	
 	T4SeaExperimental1 = {
-		Cybran = {'xrs0402', 100},		# SeaDragon (old)
-		Seraphim = {'bss0401', 100},	# Dreadnought
+		Cybran = {'xrs0402', 100}, -- SeaDragon (old)
+		Seraphim = {'bss0401', 100} -- Dreadnought
 	},
 	
 	T4SeaExperimental2 = {
-		UEF = {'bes0402', 100},			# Dreadnought
-		Cybran = {'brs0402', 100},		# SeaDragon Mk. 2 (new)
+		UEF = {'bes0402', 100},	-- Dreadnought
+		Cybran = {'brs0402', 100}, -- SeaDragon Mk. 2 (new)
 	},
 }
