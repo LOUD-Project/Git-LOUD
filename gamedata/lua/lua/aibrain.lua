a731d000f8c9872059c6e60431ca48189f88d425
Sprouto1 <loudman@rogers.com>
2021-01-22 15:47:12 -0500

    LUA - Release 5.4.02
    Some touchups to AI 
    Modmanager/Unit Database support

--- gamedata/lua/lua/aibrain.lua
+++ gamedata/lua/lua/aibrain.lua
@@ -5,12 +5,12 @@
 	-- Enable LOUD debugging options
 	LOG("*AI DEBUG Setting LOUD DEBUG & LOG options")
 
---[[    
+    
     --- ENGINEER and FACTORY DEBUGS ---
 
-
+--[[
     -- AI Engineers will be named according to the Builder they are running 
-	ScenarioInfo.NameEngineers = false
+	ScenarioInfo.NameEngineers = true
 	LOG("*AI DEBUG		Name Engineers is "..repr(ScenarioInfo.NameEngineers))
 
     -- engineers will dialog their decisions to the LOG --
@@ -116,7 +116,7 @@
 	LOG("*AI DEBUG		Display Platoon Membership is "..repr(ScenarioInfo.DisplayPlatoonMembership))
     
     -- AI will display the platoon (Buildername) over the platoon every few seconds (not crowded but must look closely)
-	ScenarioInfo.DisplayPlatoonPlans = false
+	ScenarioInfo.DisplayPlatoonPlans = true
 	LOG("*AI DEBUG		Display Platoon Plans is "..repr(ScenarioInfo.DisplayPlatoonPlans))
 
     -- AI bases and platoons that respond to distress will dialog their data and decisions to the LOG
