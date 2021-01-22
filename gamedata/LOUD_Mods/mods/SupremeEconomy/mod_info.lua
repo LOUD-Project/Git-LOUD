d40a4a323d7c339b8f5fcd7ba7955c349c205eee
TheRatCircus <bloxbuilder@gmail.com>
2021-01-22 11:49:19 -0500

    LOUDMODS: Make Supreme Eco and UIP mutually exclusive

--- gamedata/LOUD_Mods/mods/SupremeEconomy/mod_info.lua
+++ gamedata/LOUD_Mods/mods/SupremeEconomy/mod_info.lua
@@ -6,5 +6,6 @@ author = "MaCielPL"
 url = "http://supecono.googlepages.com"
 uid = "89BF1572-9EA8-11DC-1313-635F56D89591"
 
+conflict = { "022E3DB4-9C00-5ED7-9876-4866D316E015" } -- Conflicts with UI Party
 exclusive = false
 ui_only = true
