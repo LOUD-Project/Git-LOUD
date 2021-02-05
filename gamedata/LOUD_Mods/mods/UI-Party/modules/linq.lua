f8eafbbc5ba68964f7c5b8bb9d6716edd96d9e14
TheRatCircus <bloxbuilder@gmail.com>
2021-02-04 19:40:03 -0500

    UIP: Restore a needed LINQ func (and alphabetize them)

--- gamedata/LOUD_Mods/mods/UI-Party/modules/linq.lua
+++ gamedata/LOUD_Mods/mods/UI-Party/modules/linq.lua
@@ -6,43 +6,6 @@
 
 local passthroughCondition = function(k,v) return true end
 
-function Select(tab, callback)
-	local result = {}
-	for k,v in tab do
-		if callback(k,v) then
-			result[k] = callback(k,v)
-		end
-	end
-	return result
-end
-
-function Where(tab, callback)
-	local result = {}
-	for k, v in tab do
-		if callback(k, v) then
-			table.insert(result, v)
-		end
-	end
-	return result
-end
-
-function Contains(tab, value)
-	for _, v in tab do
-		if v == value then return true end
-	end
-	return false
-end
-
-function Distinct(tab)
-	local result = {}
-	for _, v in tab do
-		if not Contains(result, v) then
-			table.insert(result, v)
-		end
-	end
-	return result
-end
-
 function All(tab, callback)
 	for k, v in tab do
 		if not callback(k,v) then
@@ -52,32 +15,11 @@ function All(tab, callback)
 	return true
 end
 
-function First(tab, condition)
-	for k, v in tab do
-		if not condition or condition(k,v) then return v end
-	end
-	return nil
-end
-
-function Last(tab, condition)
-	local l = nil
-	for k, v in tab do
-		if not condition or condition(k, v) then l = v end
-	end
-	return l
-end
-
 function Any(tab, condition)
 	for k, v in tab do
 		if not condition or condition(k, v) then return true end
 	end
 	return false
-end
-
-function Count(tab, condition)
-	if not condition then condition = passthroughCondition end
-	tab = Where(tab, condition)
-	return table.getn(ToArray(tab))
 end
 
 function Average(tab, selector)
@@ -98,6 +40,53 @@ function Concat(tab1, tab2)
 	return result
 end
 
+function Contains(tab, value)
+	for _, v in tab do
+		if v == value then return true end
+	end
+	return false
+end
+
+function Count(tab, condition)
+	if not condition then condition = passthroughCondition end
+	tab = Where(tab, condition)
+	return table.getn(ToArray(tab))
+end
+
+function Distinct(tab)
+	local result = {}
+	for _, v in tab do
+		if not Contains(result, v) then
+			table.insert(result, v)
+		end
+	end
+	return result
+end
+
+function First(tab, condition)
+	for k, v in tab do
+		if not condition or condition(k,v) then return v end
+	end
+	return nil
+end
+
+function Last(tab, condition)
+	local l = nil
+	for k, v in tab do
+		if not condition or condition(k, v) then l = v end
+	end
+	return l
+end
+
+function Max(tab, selector)
+	local best = nil
+	for k, v in tab do
+		if selector then v = selector(k, v) end
+		if v > best then best = v end
+	end
+	return best
+end
+
 function RemoveByValue(tab, value)
 	for k, v in ipairs(tab) do
 		if v == value then
@@ -108,10 +97,30 @@ function RemoveByValue(tab, value)
 	LOG("Value not found: "..repr(tab))
 end
 
+function Select(tab, callback)
+	local result = {}
+	for k,v in tab do
+		if callback(k,v) then
+			result[k] = callback(k,v)
+		end
+	end
+	return result
+end
+
 function ToArray(tab)
 	local result = {}
 	for _, v in tab do
 		table.insert(result, v)
+	end
+	return result
+end
+
+function Where(tab, callback)
+	local result = {}
+	for k, v in tab do
+		if callback(k, v) then
+			table.insert(result, v)
+		end
 	end
 	return result
 end
