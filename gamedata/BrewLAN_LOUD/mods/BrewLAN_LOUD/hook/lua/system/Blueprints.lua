5c6ee52592aba7b00c3140e3775f7678c8abbda2
TheRatCircus <bloxbuilder@gmail.com>
2021-05-15 18:28:35 -0400

    BL: Stop engineering ships being built at non-naval factories

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/hook/lua/system/Blueprints.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/hook/lua/system/Blueprints.lua
@@ -165,7 +165,8 @@ function BrewLANNavalEngineerCatFixes(all_bps)
         {'BUILTBYTIER1FACTORY SERAPHIM MOBILE CONSTRUCTION', 'BUILTBYTIER1FACTORY SERAPHIM MOBILE LAND CONSTRUCTION'},
     }
     for id, bp in all_bps do
-        if bp.General.Classification == 'RULEUC_Factory' and bp.Physics.BuildOnLayerCaps.LAYER_Water == false then
+        -- If table doesn't exist, it's 'Land'. If a key doesnt exist, but the table does, that key is false.
+        if bp.General.Classification == 'RULEUC_Factory' and (bp.Physics.BuildOnLayerCaps and not bp.Physics.BuildOnLayerCaps.LAYER_Water or not bp.Physics.BuildOnLayerCaps) then
             if bp.Economy.BuildableCategory then
                 for i, cat in bp.Economy.BuildableCategory do
                     for index, cattable in cats_table do
