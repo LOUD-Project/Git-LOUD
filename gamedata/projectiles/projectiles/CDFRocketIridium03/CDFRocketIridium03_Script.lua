85f155ae8102a448ab47d3fbf7d9e1fab90f968e
Sprouto1 <loudman@rogers.com>
2021-09-24 15:53:18 -0400

    PROJECTILES - Ver 3.2.3
    Final update for Triseptitron impact decal - further reduction to LOD and lifetime value.

--- gamedata/projectiles/projectiles/CDFRocketIridium03/CDFRocketIridium03_Script.lua
+++ gamedata/projectiles/projectiles/CDFRocketIridium03/CDFRocketIridium03_Script.lua
@@ -1,17 +1,23 @@
 local CIridiumRocketProjectile = import('/lua/cybranprojectiles.lua').CIridiumRocketProjectile
+
 CDFRocketIridium03 = Class(CIridiumRocketProjectile) {
+
     OnCreate = function(self)
         CIridiumRocketProjectile.OnCreate(self)
         self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
     end,
 
     OnImpact = function(self, targetType, targetEntity)
+    
         CIridiumRocketProjectile.OnImpact(self, targetType, targetEntity)
-        local army = self:GetArmy()
-        CreateLightParticle( self, -1, army, 2, 1, 'glow_03', 'ramp_red_06' )
-        CreateLightParticle( self, -1, army, 1, 3, 'glow_03', 'ramp_antimatter_02' )
+        
+        local army = self.Army
+        
+        CreateLightParticle( self, -1, army, 1.85, 1, 'glow_03', 'ramp_red_06' )
+        CreateLightParticle( self, -1, army, 1, 2.6, 'glow_03', 'ramp_antimatter_02' )
+        
         if targetType == 'Terrain' or targetType == 'Prop' then
-            CreateDecal( self:GetPosition(), import('/lua/utilities.lua').GetRandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 2, 2, 350, 200, army )
+            CreateDecal( self:GetPosition(), import('/lua/utilities.lua').GetRandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 1.6, 1.6, 200, 160, army )
         end
     end,
 }
