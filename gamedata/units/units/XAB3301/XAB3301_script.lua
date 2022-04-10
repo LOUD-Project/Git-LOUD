d89442552a198664e0003a84110f7446f0031801
Sprouto1 <loudman@rogers.com>
2022-04-10 13:45:33 -0400

    UNITS - Eye of Rhianne
    Script error hotfix

--- gamedata/units/units/XAB3301/XAB3301_script.lua
+++ gamedata/units/units/XAB3301/XAB3301_script.lua
@@ -1,8 +1,8 @@
-local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
+local AStructureUnit = import('/lua/defaultunits.lua').StructureUnit
 local RemoteViewing = import('/lua/RemoteViewing.lua').RemoteViewing
+
 AStructureUnit = RemoteViewing( AStructureUnit )
 
-XAB3301 = Class( AStructureUnit ) {
-}
+XAB3301 = Class( AStructureUnit ) {}
 
 TypeClass = XAB3301
