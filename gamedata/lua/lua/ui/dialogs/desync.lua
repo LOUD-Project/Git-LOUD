b0da82af7c2c5f5f18088660e8ae5c9bc48ea098
Sprouto1 <loudman@rogers.com>
2022-06-22 02:30:38 -0400

    LUA - Desync message
    Once you tell the SIM to stop desync messages - that's what will happen.   No more streaming desync messages.

--- gamedata/lua/lua/ui/dialogs/desync.lua
+++ gamedata/lua/lua/ui/dialogs/desync.lua
@@ -14,36 +14,52 @@ local dialog = false
 local beforeStop = 0
 
 function UpdateDialog(beatNumber, strings)
+
     if beforeStop != false then
+    
 		if not dialog then
+        
 			dialog = Group(GetFrame(0), "updateDialogGroup")
+            
 			LOG("Desynch at beat " .. beatNumber .. " tick " .. GetGameTimeSeconds())
+            
 			dialog.Width:Set(300)
 			dialog.Height:Set(250)
 			dialog.Depth:Set(GetFrame(0):GetTopmostDepth() + 10)
+            
 			LayoutHelpers.AtCenterIn(dialog, GetFrame(0))
+            
 			local border, bg = UIUtil.CreateBorder(dialog, true)
 
 			local title = UIUtil.CreateText(bg, "<LOC desync_0000>Desync Detected", 14, UIUtil.titleFont)
+            
 			LayoutHelpers.AtTopIn(title, dialog, 5)
 			LayoutHelpers.AtHorizontalCenterIn(title, dialog)
 
 			dialog.textControls = {}            
+            
 			local prev = false
+            
 			for i = 1,9 do
+            
 				dialog.textControls[i] = UIUtil.CreateText(bg, "", 12, UIUtil.bodyFont)
+                
 				if prev then
 					LayoutHelpers.Below(dialog.textControls[i], prev, 5)
 				else
 					LayoutHelpers.AtLeftIn(dialog.textControls[i], bg, 5)
 					dialog.textControls[i].Top:Set(function() return title.Bottom() + 5 end)
 				end
+                
 				prev = dialog.textControls[i]
 			end
 			
 			local okBtn = UIUtil.CreateButtonStd(bg, '/widgets/small', "<LOC _Ok>", 10)
+            
 			okBtn.Top:Set(dialog.textControls[9].Bottom)
+            
 			LayoutHelpers.AtLeftIn(okBtn, bg)
+            
 			okBtn.OnClick = function(self, modifiers)
 				dialog:Destroy()
 				dialog = false
@@ -51,13 +67,17 @@ function UpdateDialog(beatNumber, strings)
 			end
 			
 			local StopBtn = UIUtil.CreateButtonStd(bg, '/widgets/small', "Stop Pop-Ups", 10) -- Add button by Xinnony
+            
 			StopBtn.Top:Set(dialog.textControls[9].Bottom)
+            
 			LayoutHelpers.AtRightIn(StopBtn, bg)
+            
 			if beforeStop < 1 then
 				StopBtn:Disable()
 			else
 				StopBtn:Enable()
 			end
+            
 			StopBtn.OnClick = function(self, modifiers)
 				dialog:Destroy()
 				dialog = false
@@ -70,8 +90,12 @@ function UpdateDialog(beatNumber, strings)
 				dialog.textControls[i]:SetText(strings[i])
 			end
 		end
+        
 		dialog.textControls[9]:SetText(LOC("<LOC desync_0001>Beat# ") .. tostring(beatNumber))
+        
 	else
-		PrintText('Desync', 4, UIUtil.fontColor, 1, '')
+    
+		--PrintText('Desync', 4, UIUtil.fontColor, 1, '')
+        
 	end
 end
