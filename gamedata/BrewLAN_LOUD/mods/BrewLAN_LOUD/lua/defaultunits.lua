d70702c15cfac24687ba3cfa49878cea39f7881a
Sprouto1 <loudman@rogers.com>
2023-02-01 10:11:51 -0500

    BrewLAN - Mines
    This is a general rebalance of mines based upon (but not quite the same) the same energy formulae as artillery utilizes.    This gives us a solid foundation for altering mine damage and AOE radius.
    
    I have tried to position the 3 mine types so that they are reasonably affordable - and effective - within the tier they appear.   It's obviously open for re-interpretation at some point, but this should lower the bar for wider usage.

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/lua/defaultunits.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/lua/defaultunits.lua
@@ -2,30 +2,36 @@
 -- Summary  :  BrewLAN multi-unit scripts.
 --------------------------------------------------------------------------------
 local DefaultUnits = import('/lua/defaultunits.lua')
+
 local StructureUnit = DefaultUnits.StructureUnit
+
 local FactoryUnit = DefaultUnits.FactoryUnit
 local EnergyStorageUnit = DefaultUnits.EnergyStorageUnit
---------------------------------------------------------------------------------
+
 local BrewLANLOUDPath = import( '/lua/game.lua' ).BrewLANLOUDPath()
 local DefaultWeapons = import('/lua/sim/DefaultWeapons.lua')
 local DeathNukeWeapon = import(BrewLANLOUDPath .. '/lua/sim/defaultweapons.lua').DeathNukeWeapon
 local EnergyStorageVariableDeathWeapon = import(BrewLANLOUDPath .. '/lua/weapons.lua').EnergyStorageVariableDeathWeapon
 local GetTerrainAngles = import(BrewLANLOUDPath .. '/lua/TerrainUtils.lua').GetTerrainSlopeAnglesDegrees
---------------------------------------------------------------------------------
+
 local EffectTemplates = import('/lua/effecttemplates.lua')
 local CreateScorchMarkSplat = import('/lua/defaultexplosions.lua').CreateScorchMarkSplat
 local GetRandomFloat = import('/lua/utilities.lua').GetRandomFloat
 local TableCat = import('/lua/utilities.lua').TableCat
 local EBPSRA = '/effects/emitters/seraphim_rifter_artillery_hit_'
+
 --------------------------------------------------------------------------------
 -- Mine script
 --------------------------------------------------------------------------------
 
 MineStructureUnit = Class(StructureUnit) {
+
     Weapons = {
+
         Suicide = Class(DefaultWeapons.KamikazeWeapon) {
 
             FxDeathLand = EffectTemplates.DefaultHitExplosion01,
+
             FxDeathWater = {
                 EBPSRA .. '01w_emit.bp',
                 EBPSRA .. '02w_emit.bp',
@@ -34,18 +40,22 @@ MineStructureUnit = Class(StructureUnit) {
                 EBPSRA .. '06w_emit.bp',
                 EBPSRA .. '08w_emit.bp',
             },
+
             FxDeathSeabed = EffectTemplates.DefaultProjectileWaterImpact,
 
             OnFire = function(self)
+
                 local bp = self:GetBlueprint()
                 local radius = bp.DamageRadius
                 local army = self.unit:GetArmy()
                 local pos = self.unit:GetPosition()
+
                 local FxDeath = {
                     Land = self.FxDeathLand,
                     Water = self.FxDeathWater,
                     Seabed = TableCat(self.FxDeathSeabed, self.FxDeathLand),
                 }
+
                 for layer, effects in FxDeath do
                     if layer == self.unit:GetCurrentLayer() then
                         for k, v in effects do
@@ -53,19 +63,25 @@ MineStructureUnit = Class(StructureUnit) {
                         end
                     end
                 end
+
                 if not self.SplatTexture then
                     CreateScorchMarkSplat( self.unit, bp.DamageRadius * 0.5, army)
                 else
+
                     if self.SplatTexture.Albedo then                                                                                                                                                                                    --LOD          Lifetime
                         CreateDecal( pos, GetRandomFloat(0,2*math.pi), (self.SplatTexture.Albedo[1] or self.SplatTexture.Albedo), '', 'Albedo', radius * (self.SplatTexture.Albedo[2] or 4), radius * (self.SplatTexture.Albedo[2] or 4), radius * 60, bp.Damage * 15, army )
                     end
+
                     if self.SplatTexture.AlphaNormals then
                         CreateDecal( pos, GetRandomFloat(0,2*math.pi), (self.SplatTexture.AlphaNormals[1] or self.SplatTexture.AlphaNormals), '', 'Alpha Normals', radius * (self.SplatTexture.AlphaNormals[2] or 2), radius * (self.SplatTexture.AlphaNormals[2] or 2), radius * 30, bp.Damage * 15, army )
                     end
                 end
-                DamageArea(self.unit, pos, radius * 0.5, 1, 'Force', true)
-                DamageArea(self.unit, pos, radius * 0.5, 1, 'Force', true)
-                DamageRing(self.unit, pos, 0.1, radius, 10, 'Fire', false, false)
+
+                DamageArea( self.unit, pos, radius * 0.5, 1, 'Force', true)
+                DamageArea( self.unit, pos, radius * 0.5, 1, 'Force', true)
+
+                DamageRing( self.unit, pos, 0.1, radius, 10, 'Fire', false, false)
+
                 self.unit.DeathWeaponEnabled = false
                 DefaultWeapons.KamikazeWeapon.OnFire(self)
             end,
@@ -73,43 +89,43 @@ MineStructureUnit = Class(StructureUnit) {
     },
 
     OnCreate = function(self,builder,layer)
+
         StructureUnit.OnCreate(self)
-        --enable cloaking and stealth
-        self:EnableIntel('Cloak')
-        self:EnableIntel('RadarStealth')
-        self:EnableIntel('SonarStealth')
+
         local pos = self:GetPosition()
+
         self.blocker = CreateUnitHPR('ZZZ2220',self:GetArmy(),pos[1],pos[2],pos[3],0,0,0)
+
         self.Trash:Add(self.blocker)
     end,
 
     OnStopBeingBuilt = function(self,builder,layer)
+
         StructureUnit.OnStopBeingBuilt(self,builder,layer)
+        
         local bp = self:GetBlueprint()
+
         if self:GetCurrentLayer() == 'Water' then
             self.Trash:Add(CreateSlider(self, 0, 0, -1 * 1 / bp.Display.UniformScale, 0, 5))
         end
+
+        --This tricks the engine into thinking the area is clear:
+        --Removing a building with an overlapping footprint from the same layer.
         if self.blocker then
-            --This tricks the engine into thinking the area is clear:
-            --Removing a building with an overlapping footprint from the same layer.
             self.blocker:Destroy()
+            self.blocker = nil
         end
-        --Force update of the cloak effect if there is a cloak mesh. For FAF graphics
-        if self:GetBlueprint().Display.CloakMeshBlueprint then
-            self:ForkThread(
-                function()
-                    WaitTicks(1)
-                    self:UpdateCloakEffect(true, 'Cloak')
-                end
-            )
-        end
+
     end,
 
     OnScriptBitSet = function(self, bit)
+
         StructureUnit.OnScriptBitSet(self, bit)
+
         if bit == 1 then
             self:GetWeaponByLabel('Suicide'):FireWeapon()
         end
+
     end,
 
     OnKilled = function(self, instigator, type, overkillRatio)
@@ -122,18 +138,23 @@ MineStructureUnit = Class(StructureUnit) {
 --------------------------------------------------------------------------------
 
 NukeMineStructureUnit = Class(MineStructureUnit) {
+
     Weapons = {
+
         DeathWeapon = Class(DefaultWeapons.BareBonesWeapon) {
+ 
             OnFire = function(self)
             end,
 
             Fire = function(self)
                 local myBlueprint = self:GetBlueprint()
                 local myProjectile = self.unit:CreateProjectile(myBlueprint.ProjectileId, 0, 0, 0, nil, nil, nil):SetCollision(false)
-                myProjectile:PassDamageData(self:GetDamageTable())
+                myProjectile:PassDamageData(self.damageTable)
             end,
         },
+
         Suicide = Class(DeathNukeWeapon) {
+
             Fire = function(self, ...)
                 local radius = self:GetBlueprint().NukeInnerRingRadius or self:GetBlueprint().DamageRadius
                 local pos = self.unit:GetPosition()
