adcdcaad1f1d9191f8e6583b60a7758f573c09bc
Sprouto1 <loudman@rogers.com>
2023-02-24 23:05:11 -0500

    LOUD_MISC - Enhance Static HP
    Added this mod, with expanded options
    1.25, 1.5, 1.75, 2, 2.5

---
+++ gamedata/LOUD_Misc/mods/Enhanced Static HP/config.lua
@@ -0,0 +1,29 @@
+config = {
+	{   default = 1,
+        label = "Multiplier",
+        key = 'Multiplier',
+        values = {
+            {
+                text = "x1.25",
+                key = '1.25',
+            },
+            {
+                text = "x1.5",
+                key = '1.5',
+            },
+            {
+                text = "x1.75",
+                key = '1.75',
+            },
+            {
+                text = "x2.0",
+                key = '2.0',
+            },
+            {
+                text = "x2.5",
+                key = '2.5',
+            },
+        },
+    },
+   
+}
---
+++ gamedata/LOUD_Misc/mods/Enhanced Static HP/mod_info.lua
@@ -0,0 +1,11 @@
+name = "Enhanced Static HP"
+uid = "b8d941e131multiplier"
+version = 6.3
+copyright = ""
+description = "All static units have health and shield points multiplied by number noted in config."
+author = "Balthazar (edit by Sad Turtle Man)"
+
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
