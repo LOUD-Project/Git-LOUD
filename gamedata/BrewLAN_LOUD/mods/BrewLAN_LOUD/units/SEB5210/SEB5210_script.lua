cd84f7133b3f3f5658c0d10a60f8c874529cc21b
Sprouto1 <loudman@rogers.com>
2023-05-06 08:59:49 -0400

    BrewLAN for LOUD - UEF T3 Wall
    Missed a changed class - fixed now.

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SEB5210/SEB5210_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SEB5210/SEB5210_script.lua
@@ -1,4 +1,4 @@
-local TWallStructureUnit = import('/defaultunits.lua').WallStructureUnit
+local TWallStructureUnit = import('/lua/defaultunits.lua').WallStructureUnit
 
 local CardinalWallUnit = import(import( '/lua/game.lua' ).BrewLANLOUDPath() .. '/lua/walls.lua').CardinalWallUnit
 
