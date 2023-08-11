9e0ce948562f667a18e4fbae600b9c16cd404beb
Sprouto1 <loudman@rogers.com>
2023-08-11 16:25:42 -0400

    UNITS - Aeon T2 TMD rework
    This reworks the Aeon TMD, both visually (missing animations), and technically - it's firing sequence, unpacking and the way it's projectile uses the FLARE.

--- gamedata/units/units/UAB4201/UAB4201_script.lua
+++ gamedata/units/units/UAB4201/UAB4201_script.lua
@@ -3,33 +3,39 @@ local AStructureUnit = import('/lua/defaultunits.lua').StructureUnit
 local AAMWillOWisp = import('/lua/aeonweapons.lua').AAMWillOWisp
 
 UAB4201 = Class(AStructureUnit) {
-    Weapons = {
-        AntiMissile = Class(AAMWillOWisp) {
-                PlayRackRecoil = function(self, rackList)
-                    AAMWillOWisp.PlayRackRecoil(self, rackList)
-                     #CreateRotator(unit, bone, axis, [goal], [speed], [accel], [goalspeed])
-                    if not self.RotatorManipulator then
-                        self.RotatorManipulator = CreateRotator(self.unit, 'Dome', 'z', 20, 40, 40, 40)
-                        self.RotatorManipulator:SetGoal(45)
-                        self.RotatorManipulatorCounter = 1
-                    else
-                        self.RotatorManipulatorCounter = self.RotatorManipulatorCounter + 1
-                        self.RotatorManipulator:SetGoal(45 * self.RotatorManipulatorCounter)
-                    end
-                    self.unit.Trash:Add(self.RotatorManipulator)
-                end,
-   
-   PlayRackRecoilReturn = function(self, rackList)
-                    AAMWillOWisp.PlayRackRecoilReturn(self, rackList)
-                    if self.RotatorManipulatorCounter == 8 then
-                        self.RotatorManipulator:Destroy()
-                        self.RotatorManipulator = nil
-                    end
-                end,
-            
-            },
-    },
 
+    Weapons = {
+
+        AntiMissile = Class(AAMWillOWisp) {
+
+            PlayRackRecoil = function(self, rackList)
+
+                AAMWillOWisp.PlayRackRecoil(self, rackList)
+
+                --CreateRotator(unit, bone, axis, [goal], [speed], [accel], [goalspeed])
+                if not self.RotatorManipulator then
+                    self.RotatorManipulator = CreateRotator(self.unit, 'Dome', 'z', 20, 40, 40, 40)
+                    self.RotatorManipulator:SetGoal(45)
+                    self.RotatorManipulatorCounter = 1
+                else
+                    self.RotatorManipulatorCounter = self.RotatorManipulatorCounter + 1
+                    self.RotatorManipulator:SetGoal(45 * self.RotatorManipulatorCounter)
+                end
+
+                self.unit.Trash:Add(self.RotatorManipulator)
+            end,
+   
+            PlayRackRecoilReturn = function(self, rackList)
+            
+                AAMWillOWisp.PlayRackRecoilReturn(self, rackList)
+
+                if self.RotatorManipulatorCounter == 8 then
+                    self.RotatorManipulator:Destroy()
+                    self.RotatorManipulator = nil
+                end
+            end,
+        },
+    },
 }
 
 TypeClass = UAB4201
