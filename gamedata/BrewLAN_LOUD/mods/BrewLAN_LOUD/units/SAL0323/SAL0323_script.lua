6e23bdd14ebec9b78f9b4860df5dd62476a982e4
Sprouto1 <loudman@rogers.com>
2023-08-26 18:32:54 -0400

    UNITS, BL, TM - ASF attacking factories
    Following thru on other fighters attacking AIR factories.

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAL0323/SAL0323_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAL0323/SAL0323_script.lua
@@ -3,14 +3,17 @@ local AStructureUnit = import('/lua/defaultunits.lua').StructureUnit
 local AAMWillOWisp = import('/lua/aeonweapons.lua').AAMWillOWisp
 
 SAL0323 = Class(AStructureUnit) {
+
     Weapons = {
+
         AntiMissile = Class(AAMWillOWisp) {
         
                 PlayRackRecoil = function(self, rackList)
+
                     AAMWillOWisp.PlayRackRecoil(self, rackList)
 
                     if not self.RotatorManipulator then
-                        self.RotatorManipulator = CreateRotator(self.unit, 'Dome', 'z', 20, 40, 40, 40)
+                        self.RotatorManipulator = CreateRotator(self.unit, 'Barrel', 'z', 20, 40, 40, 40)
                         self.RotatorManipulator:SetGoal(45)
                         self.RotatorManipulatorCounter = 1
                     else
@@ -22,7 +25,9 @@ SAL0323 = Class(AStructureUnit) {
                 end,
                 
                 PlayRackRecoilReturn = function(self, rackList)
+
                     AAMWillOWisp.PlayRackRecoilReturn(self, rackList)
+
                     if self.RotatorManipulatorCounter == 8 then
                         self.RotatorManipulator:Destroy()
                         self.RotatorManipulator = nil
