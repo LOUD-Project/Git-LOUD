1ed4f4329fd90a898d09dbdec5ba48e2f352c391
Sprouto1 <loudman@rogers.com>
2023-11-12 17:02:56 -0500

    UNITS - Cybran T2 Amphib tank
    This unit had some script that was disabling weapons based on a LayerChange() event - problem is simple - once disabled via this, it wasn't being re-enabled properly.
    
    The base weapon code handles this anyhow making it redundant.
    
    Some weapon review in order as well, the default shield damage was too high, and the rocket velocity and aim bones were wrong.

--- gamedata/units/units/URL0203/URL0203_script.lua
+++ gamedata/units/units/URL0203/URL0203_script.lua
@@ -1,6 +1,7 @@
-
 local CLandUnit = import('/lua/defaultunits.lua').MobileUnit
+
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
+
 local CDFElectronBolterWeapon = CybranWeaponsFile.CDFElectronBolterWeapon
 local CDFMissileMesonWeapon = CybranWeaponsFile.CDFMissileMesonWeapon
 local CANTorpedoLauncherWeapon = CybranWeaponsFile.CANTorpedoLauncherWeapon
@@ -13,42 +14,5 @@ URL0203 = Class(CLandUnit) {
         Torpedo = Class(CANTorpedoLauncherWeapon) {},
     },
     
-    OnStopBeingBuilt = function(self, builder, layer)
-        CLandUnit.OnStopBeingBuilt(self,builder,layer)
-        # If created with F2 on land, then play the transform anim.
-        if(self:GetCurrentLayer() == 'Land') then
-			# Enable Land weapons
-	        self:SetWeaponEnabledByLabel('Rocket', true)
-	        self:SetWeaponEnabledByLabel('Bolter', true)
-			# Disable Torpedo
-	        self:SetWeaponEnabledByLabel('Torpedo', false)
-        elseif (self:GetCurrentLayer() == 'Seabed') then
-			# Disable Land Weapons
-	        self:SetWeaponEnabledByLabel('Rocket', false)
-	        self:SetWeaponEnabledByLabel('Bolter', false)
-			# Enable Torpedo
-	        self:SetWeaponEnabledByLabel('Torpedo', true)
-        end
-       self.WeaponsEnabled = true
-    end,
-
-	OnLayerChange = function(self, new, old)
-		CLandUnit.OnLayerChange(self, new, old)
-		if self.WeaponsEnabled then
-			if( new == 'Land' ) then
-				# Enable Land weapons
-				self:SetWeaponEnabledByLabel('Rocket', true)
-				self:SetWeaponEnabledByLabel('Bolter', true)
-				# Disable Torpedo
-				self:SetWeaponEnabledByLabel('Torpedo', false)
-			elseif ( new == 'Seabed' ) then
-				# Disable Land Weapons
-				self:SetWeaponEnabledByLabel('Rocket', false)
-				self:SetWeaponEnabledByLabel('Bolter', false)
-				# Enable Torpedo
-				self:SetWeaponEnabledByLabel('Torpedo', true)
-			end
-		end
-	end,
 }
 TypeClass = URL0203
