3e4242718ad4975679b7f1846f9671d1c21f34ea
Sprouto1 <loudman@rogers.com>
2023-12-08 12:35:09 -0500

    BrewLAN for LOUD - ver 0.8.8.9.77
    Release point

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAB2222/SAB2222_Script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAB2222/SAB2222_Script.lua
@@ -2,5 +2,7 @@
 --  Summary  :  Aeon Stationary Explosive Script
 --------------------------------------------------------------------------------
 local NukeMineStructureUnit = import(import( '/lua/game.lua' ).BrewLANLOUDPath() .. '/lua/defaultunits.lua').NukeMineStructureUnit
+
 SAB2222 = Class(NukeMineStructureUnit) {}
+
 TypeClass = SAB2222
