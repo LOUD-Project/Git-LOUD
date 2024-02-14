b3d14ff01d3b11748e2bf71b4cc48d6c396f4214
Sprouto1 <loudman@rogers.com>
2024-02-14 18:01:56 -0500

    4DC - Beholder T3 Point Defense
    Fixed an issue with the beam
    Additional firing effects
    Rebalanced

--- gamedata/4D-CompatabilityPack/mods/4DC/units/URB2306/URB2306_Script.lua
+++ gamedata/4D-CompatabilityPack/mods/4DC/units/URB2306/URB2306_Script.lua
@@ -3,10 +3,47 @@ local CStructureUnit = import('/lua/defaultunits.lua').StructureUnit
 local CDFLaserHeavyWeapon = import('/lua/cybranweapons.lua').CDFLaserHeavyWeapon
 local CDFHeavyMicrowaveLaserGenerator = import('/lua/cybranweapons.lua').CDFHeavyMicrowaveLaserGenerator
 
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+
+local CreateAttachedEmitter = CreateAttachedEmitter
+
 URB2306 = Class(CStructureUnit) {
     Weapons = {
-        MainGun = Class(CDFHeavyMicrowaveLaserGenerator) {},
-        Laser = Class(CDFLaserHeavyWeapon) {}
+    
+        BeamCannon = Class(CDFHeavyMicrowaveLaserGenerator) {
+
+            FxMuzzleFlashScale = 1.25,
+
+            FxMuzzleFlash = { 
+            	'/effects/emitters/proton_artillery_muzzle_01_emit.bp',
+            	'/effects/emitters/proton_artillery_muzzle_03_emit.bp',
+                '/effects/emitters/cybran_artillery_muzzle_smoke_01_emit.bp',                                
+            },
+			
+	        FxMuzzleEffect = EffectTemplate.CIFCruiseMissileLaunchSmoke,
+			
+	        FxVentEffect = EffectTemplate.WeaponSteam01,			
+	        FxVentEffect2 = EffectTemplate.CElectronBolterMuzzleFlash01,
+			
+	        PlayFxMuzzleSequence = function(self, muzzle)
+
+		        local army = self.unit:GetArmy()
+		        
+  	            for k, v in self.FxMuzzleEffect do
+                    CreateAttachedEmitter(self.unit, 'LaserTurretSleeve', army, v):ScaleEmitter(1.5)
+                end
+
+  	            for k, v in self.FxVentEffect do
+                    CreateAttachedEmitter(self.unit, 'Sleeve', army, v):ScaleEmitter(1.5)
+                end				
+				
+  	            for k, v in self.FxVentEffect2 do
+                    CreateAttachedEmitter(self.unit, muzzle, army, v):ScaleEmitter(1)
+                end
+            end, 
+        },
+
+        AutoCannon = Class(CDFLaserHeavyWeapon) {}
     },
 }
 TypeClass = URB2306
