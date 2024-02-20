fbe29c10874a3866f363104de98c5b64ba6ea94e
Sprouto1 <loudman@rogers.com>
2024-02-20 17:34:12 -0500

    UNITS - Brick converted to simple anti-torpedo
    Brick was using the anti-torp flare - but it's now been converted to the simple anti-torpedo torpedo.
    
    The torpedo launcher has now also been converted to a single 100 damage torpedo, from it's previous 2 x 50, and range has been trimmed from 40 to 35.

--- gamedata/units/units/XRL0305/XRL0305_Script.lua
+++ gamedata/units/units/XRL0305/XRL0305_Script.lua
@@ -1,16 +1,22 @@
 local CWalkingLandUnit = import('/lua/defaultunits.lua').WalkingLandUnit
+
+local AeonWeapons = import('/lua/aeonweapons.lua')
 local CWeapons = import('/lua/cybranweapons.lua')
 
 local CDFHeavyDisintegratorWeapon = CWeapons.CDFHeavyDisintegratorWeapon
 local CANNaniteTorpedoWeapon = import('/lua/cybranweapons.lua').CANNaniteTorpedoWeapon
-local CIFSmartCharge = import('/lua/cybranweapons.lua').CIFSmartCharge
+
+local AntiTorp = AeonWeapons.AIFQuasarAntiTorpedoWeapon
 
 XRL0305 = Class(CWalkingLandUnit)
 {
     Weapons = {
+
         Disintigrator = Class(CDFHeavyDisintegratorWeapon) {},
+
         Torpedo = Class(CANNaniteTorpedoWeapon) {},
-        AntiTorpedo = Class(CIFSmartCharge) {},
+
+        AntiTorpedo = Class(AntiTorp) {},
     },
 }
 
