bf90c2ca022a38973994403eed642b4f5f4ccf4e
Sprouto1 <loudman@rogers.com>
2024-06-11 13:17:50 -0400

    Projectiles - Nukes
    Submarine Nukes (and Sera BS) are now the same projectile as static nuke launchers
    
    Removed sub launched projectiles
    
    The mobile launchers have only 60% of the build power of the static launchers - so build time will be longer than static - but that's not a surprise.

--- gamedata/projectiles/projectiles/AIFQuantumWarhead01/AIFQuantumWarhead01_script.lua
+++ gamedata/projectiles/projectiles/AIFQuantumWarhead01/AIFQuantumWarhead01_script.lua
@@ -3,14 +3,15 @@ local AQuantumWarheadProjectile = import('/lua/aeonprojectiles.lua').AQuantumWar
 local ForkThread = ForkThread
 local KillThread = KillThread
 local WaitSeconds = WaitSeconds
-local VDist2 = VDist2
 
 AIFQuantumWarhead01 = Class(AQuantumWarheadProjectile) {
 
     OnImpact = function(self, TargetType, TargetEntity)
+
         if not TargetEntity or not EntityCategoryContains(categories.PROJECTILE, TargetEntity) then
-            # Play the explosion sound
+
             local myBlueprint = self:GetBlueprint()
+
             if myBlueprint.Audio.Explosion then
                 self:PlaySound(myBlueprint.Audio.Explosion)
             end
@@ -19,33 +20,43 @@ AIFQuantumWarhead01 = Class(AQuantumWarheadProjectile) {
             nukeProjectile:PassDamageData(self.DamageData)
             nukeProjectile:PassData(self.Data)
         end
+
         AQuantumWarheadProjectile.OnImpact(self, TargetType, TargetEntity)
     end,
 
     DoTakeDamage = function(self, instigator, amount, vector, damageType)
+
         if self.ProjectileDamaged then
             for k,v in self.ProjectileDamaged do
                 v(self)
             end
         end
+
         AQuantumWarheadProjectile.DoTakeDamage(self, instigator, amount, vector, damageType)
     end,
 
     OnCreate = function(self)
+
         AQuantumWarheadProjectile.OnCreate(self)
+
         local launcher = self:GetLauncher()
+
         if launcher and not launcher:IsDead() and launcher.EventCallbacks.ProjectileDamaged then
+
             self.ProjectileDamaged = {}
+
             for k,v in launcher.EventCallbacks.ProjectileDamaged do
                 table.insert( self.ProjectileDamaged, v )
             end
         end
+
         self:SetCollisionShape('Sphere', 0, 0, 0, 5.0)
-        self.MovementTurnLevel = 1
+
         self:ForkThread( self.MovementThread )
     end,
 
     MovementThread = function(self)
+
         local army = self:GetArmy()
         local launcher = self:GetLauncher()
 
