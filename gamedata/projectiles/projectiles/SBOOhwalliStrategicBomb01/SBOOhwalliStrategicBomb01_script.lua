f4cb05d286cab6392622cc2fddd3519f3709cd9d
Sprouto1 <loudman@rogers.com>
2024-06-22 19:06:21 -0400

    PROJECTILES - Version 7.01
    Brought Ahwassa bomb projectile in house

--- gamedata/projectiles/projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
+++ gamedata/projectiles/projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
@@ -1,11 +1,15 @@
 local SOhwalliStrategicBombProjectile = import('/lua/seraphimprojectiles.lua').SOhwalliStrategicBombProjectile
 
 SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile){
+
     OnImpact = function(self, TargetType, TargetEntity)
+
         SOhwalliStrategicBombProjectile.OnImpact(self, TargetType, TargetEntity)
+
         if TargetType == 'Shield' then
             return
         end
+
         self:CreateProjectile('/effects/entities/SBOOhwalliBombEffectController01/SBOOhwalliBombEffectController01_proj.bp', 0, 0, 0, 0, 0, 0):SetCollision(false)
     end,
 }
