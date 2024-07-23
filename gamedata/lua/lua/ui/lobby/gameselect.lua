46db369323b7684fcf292d58d9a53b064a204429
Niriak <niriak@gmx.de>
2024-07-23 17:27:12 +0200

    Fix lobby crash

--- gamedata/lua/lua/ui/lobby/gameselect.lua
+++ gamedata/lua/lua/ui/lobby/gameselect.lua
@@ -844,7 +844,7 @@ function CreateUI(over, exitBehavior, useSteam)
 		LayoutHelpers.AtRightBottomIn(gameListObjects[index].mapglow, gameListObjects[index].preview, -3, -3)
         gameListObjects[index].mapglow:DisableHitTest()
         
-        gameListObjects[index].nopreview = UIUtil.CreateText(gameListObjects[index], '?', 60, UIUtil.bodyFont)
+        gameListObjects[index].nopreview = UIUtil.CreateText(gameListObjects[index], '?', 16, UIUtil.bodyFont)
 		
         LayoutHelpers.AtCenterIn(gameListObjects[index].nopreview, gameListObjects[index].preview)
 		
