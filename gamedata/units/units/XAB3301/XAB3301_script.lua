05ef8b5f349f8b4d29bb70eb656e3fe3ea06ada6
Sprouto1 <loudman@rogers.com>
2025-04-13 20:42:45 -0400

    UNITS - XAB3301 - Eye of Rhianne
    No longer requires 'maintenance' energy.
    
    The unit will charge, prior to firing, with the total energy cost being spread over the 'reactivation' time and cannot be activated until the 'charge' is complete - shown by progress bar.
    
    The 'targeting' icon will only appear when the unit is charged - and the icon will disappear when the unit is 'fired'.
    
    When charged, the unit will spin slowly and have several ambient glow entities.
    
    Unit no longer fires repeatedly - all 'scrying' is manual.   May revisit the 'repeat' option at a later date.

--- gamedata/units/units/XAB3301/XAB3301_script.lua
+++ gamedata/units/units/XAB3301/XAB3301_script.lua
@@ -1,8 +1,45 @@
 local AStructureUnit = import('/lua/defaultunits.lua').StructureUnit
+
+local FxAmbient = import('/lua/effecttemplates.lua').AResourceGenAmbient
+
 local RemoteViewing = import('/lua/RemoteViewing.lua').RemoteViewing
 
 AStructureUnit = RemoteViewing( AStructureUnit )
 
-XAB3301 = Class( AStructureUnit ) {}
+XAB3301 = Class( AStructureUnit ) {
+
+    OnStopBeingBuilt = function(self, builder, layer)
+    
+        self.EmitterEffects = {}
+        
+        self.Rotator = CreateRotator(self, 'XAB3301', 'y', nil, 0, 4, 0)
+        
+        self.Trash:Add(self.Rotator)
+        
+        AStructureUnit.OnStopBeingBuilt(self,builder,layer)
+        
+    end,
+
+    RechargeEmitter = function(self)
+    
+        self.Rotator:SetTargetSpeed(0)
+    
+        for _,key in self.EmitterEffects do
+            key:Destroy()
+        end
+    
+        AStructureUnit.RechargeEmitter(self)
+        
+        self.Rotator:SetTargetSpeed(16)
+        
+        for k,v in FxAmbient do
+            table.insert( self.EmitterEffects, CreateAttachedEmitter( self, 'TargetBone01', self:GetArmy(), v ):ScaleEmitter(.25) )
+            table.insert( self.EmitterEffects, CreateAttachedEmitter( self, 'TargetBone02', self:GetArmy(), v ):ScaleEmitter(.25) )
+            table.insert( self.EmitterEffects, CreateAttachedEmitter( self, 'TargetBone03', self:GetArmy(), v ):ScaleEmitter(.25) )
+            table.insert( self.EmitterEffects, CreateAttachedEmitter( self, 'TargetBone04', self:GetArmy(), v ):ScaleEmitter(.25) )
+        end
+    
+    end,
+}
 
 TypeClass = XAB3301
