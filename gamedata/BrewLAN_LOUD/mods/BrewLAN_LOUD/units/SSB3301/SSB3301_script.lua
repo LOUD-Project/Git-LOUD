14285b73ac2597ef2b5216591b5408416d74e338
Sprouto1 <loudman@rogers.com>
2025-05-03 12:01:22 -0400

    BrewLAN - SSB3301 - Remote Viewing
    Revised for new remote viewing mechanics, it will now charge before firing - and no longer has a sustained maintenance cost
    
    Viewtime 80s >> 60s
    Reactivation 24s >> 18s
    ViewRadius 28 >> 20

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SSB3301/SSB3301_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SSB3301/SSB3301_script.lua
@@ -1,9 +1,59 @@
 local SStructureUnit = import('/lua/defaultunits.lua').StructureUnit
 
-local RemoteViewing = import(import( '/lua/game.lua' ).BrewLANLOUDPath() .. '/lua/RemoteViewing.lua').RemoteViewing
+local FxAmbient = import('/lua/effecttemplates.lua').AResourceGenAmbient
+
+local RemoteViewing = import('/lua/RemoteViewing.lua').RemoteViewing
 
 SStructureUnit = RemoteViewing( SStructureUnit )
 
-SSB3301 = Class( SStructureUnit ) {}
+SSB3301 = Class( SStructureUnit ) {
+ 
+    OnTargetLocation = function(self, location) 
+
+        local aiBrain = self:GetAIBrain()            
+
+        if self:AntiTeleportBlock( aiBrain, location) then
+            return
+        end
+
+        --LOG("*AI DEBUG BEFORE creation data is "..repr(self.RemoteViewingData))
+
+        -- find a target unit 
+        local targettable = aiBrain:GetUnitsAroundPoint(categories.SELECTABLE, location, 10)
+        local targetunit = targettable[1]
+
+        if table.getn(targettable) > 1 then
+
+            local dist = 100
+
+            for i, target in targettable do    
+
+                if IsUnit(target) then
+
+                    local cdist = VDist2Sq(target:GetPosition()[1], target:GetPosition()[3], location[1], location[3])
+
+                    if cdist < dist then
+                        dist = cdist
+                        targetunit = target
+                    end
+                end
+            end   
+        end
+
+        if targetunit and IsUnit(targetunit) then
+                
+            self.RemoteViewingData.VisibleLocation = location
+            self.RemoteViewingData.TargetUnit = targetunit
+                
+            self:CreateVisibleEntity()
+        else
+            
+            self.RemoteViewingData.VisibleLocation = false
+
+        end
+
+    end,
+
+}
 
 TypeClass = SSB3301
