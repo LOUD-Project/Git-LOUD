155819d6938f4466980ddf4526c7c701167520be
Sprouto1 <loudman@rogers.com>
2025-05-03 11:53:45 -0400

    UNITS - Mavor decreasing randomness

--- gamedata/units/units/UEB2401/UEB2401_script.lua
+++ gamedata/units/units/UEB2401/UEB2401_script.lua
@@ -15,7 +15,11 @@ UEB2401 = Class(TStructureUnit) {
                 OnGotTarget = function(self)
 				
                     TIFArtilleryWeapon.IdleState.OnGotTarget(self)
-					
+
+                    if not self.unit:IsDead() then 
+                        self.ShotCounter = GetGameTimeSeconds() 
+                    end					
+
                     if not self.ArtyAnim then
 					
                         self.ArtyAnim = CreateAnimator(self.unit)
@@ -25,6 +29,27 @@ UEB2401 = Class(TStructureUnit) {
                     end
                 end,
             },
+
+            OnLostTarget = function(self) 
+
+                if not self.unit:IsDead() then 
+                    self.ShotCounter = GetGameTimeSeconds() 
+                end 
+
+                TIFArtilleryWeapon.OnLostTarget(self) 
+            end, 
+
+            CreateProjectileAtMuzzle = function(self, muzzle)
+
+                local proj = TIFArtilleryWeapon.CreateProjectileAtMuzzle(self, muzzle)
+
+                self:SetFiringRandomness((self:GetBlueprint().FiringRandomness * (math.sin(GetGameTimeSeconds()/20)*.3 + 1)) * math.max(2 - (math.max(GetGameTimeSeconds() - self.ShotCounter, 60)-60)/60, .25 ) )
+
+                --LOG("Randomness: " .. self:GetFiringRandomness())
+                --LOG("Base rand: " .. self:GetBlueprint().FiringRandomness * (math.sin(GetGameTimeSeconds()/20)*.3 + 1))
+                --LOG("Timer mult: " .. math.max(2 - (math.max(GetGameTimeSeconds() - self.ShotCounter, 60)-60)/60, .25 ))
+            end,
+
         },
     },
 }
