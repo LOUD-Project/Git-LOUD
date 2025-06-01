0aacec808793bce414ea0709d7162aa7885cc89a
Sprouto1 <loudman@rogers.com>
2025-06-01 17:19:55 -0400

    Wyvern for LOUD - WEL0207
    Mobile TMD some blueprint cleanup
    
    Acceleration 1.1 >> 1.5
    Reverse Speed 2 >> 3
    TurnRate 40 >> 38
    TurnRadius 2 >> 6
    TurretYaw 90 >> 72
    
    Added unit to AI build tables for T2LandMobileTMD and T3LandMobileTMD
    
    Renamed T3Battlecruiser definition to T3SeaCruiser - for consistency

--- gamedata/WyvernBattlePack/mods/BattlePack/units/WEL0207/WEL0207_script.lua
+++ gamedata/WyvernBattlePack/mods/BattlePack/units/WEL0207/WEL0207_script.lua
@@ -1,12 +1,12 @@
 local TLandUnit = import('/lua/defaultunits.lua').MobileUnit
 
-local WeaponsFile = import('/lua/terranweapons.lua')
-
-local TDFHiroPlasmaCannon = WeaponsFile.TDFHiroPlasmaCannon
+local TDFHiroPlasmaCannon = import('/lua/terranweapons.lua').TDFHiroPlasmaCannon
 
 WEL0207 = Class(TLandUnit) {
+
     Weapons = {
         TMD = Class(TDFHiroPlasmaCannon) {},
     },
+
 }
 TypeClass = WEL0207
