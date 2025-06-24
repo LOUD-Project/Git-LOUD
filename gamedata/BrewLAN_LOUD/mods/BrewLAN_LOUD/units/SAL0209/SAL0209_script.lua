dd76747bf213fe8e0c52fbbab575c2cb6c65b3dc
Sprouto1 <loudman@rogers.com>
2025-06-23 22:13:23 -0400

    BrewLAN for LOUD - Aeon Field Engineer
    The shield regen start times are now normalized at 2, as with other mobile shield units.
    
    Added backup distance parameters
    
    Increased E consumption of shield

--- gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAL0209/SAL0209_script.lua
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SAL0209/SAL0209_script.lua
@@ -1,6 +1,7 @@
 local AConstructionUnit = import(import( '/lua/game.lua' ).BrewLANLOUDPath() .. '/units/sal0319/sal0319_script.lua').SAL0319
 
 SAL0209 = Class(AConstructionUnit) { 
+
     OnCreate = function( self ) 
         AConstructionUnit.OnCreate(self)
         self:HideBone('Tube003', true)
