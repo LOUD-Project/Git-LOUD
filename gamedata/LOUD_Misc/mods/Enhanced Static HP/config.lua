703fcc97b110a65a4b4d9c3713c5eb041d92f2a1
Sprouto1 <loudman@rogers.com>
2025-08-16 18:50:28 -0400

    LOUD_MISC - Static HP mod
    I've reworked the Static HP mod to be just that - Static HP - and no longer impacts the relationship between shields and weapons.
    
    The values are now dynamically handled in the blueprint, allow us to expand the range of choices.   I've included a number of options starting at below 1.0 - and up to 2.0, which is now the maximum limit.
    
    I've chosen 2.0 as the limit primarily because it so greatly impacts the relationship of mobile units to static units.  2.0x is a drastic change to the overall game balance, and should be more than sufficient.

--- gamedata/LOUD_Misc/mods/Enhanced Static HP/config.lua
+++ gamedata/LOUD_Misc/mods/Enhanced Static HP/config.lua
@@ -1,28 +1,49 @@
 config = {
-	{   default = 1,
+	{   default = 3,
         label = "Multiplier",
         key = 'Multiplier',
         values = {
             {
-                text = "x1.25",
-                key = '1.25',
+                text = "x0.75",
+                key = '0.75',
+            },
+            {
+                text = "x0.9",
+                key = '0.9',
+            },
+            {
+                text = "x1.0",
+                key = '1.0',
+            },            
+            {
+                text = "x1.1",
+                key = '1.1',
+            },
+            {
+                text = "x1.2",
+                key = '1.2',
+            },
+            {
+                text = "x1.3",
+                key = '1.3',
             },
             {
                 text = "x1.5",
                 key = '1.5',
             },
             {
-                text = "x1.75",
-                key = '1.75',
+                text = "x1.7",
+                key = '1.7',
+            },
+            {
+                text = "x1.85",
+                key = '1.85',
             },
             {
                 text = "x2.0",
                 key = '2.0',
             },
-            {
-                text = "x2.5",
-                key = '2.5',
-            },
+
         },
     },
    
--- gamedata/LOUD_Misc/mods/Enhanced Static HP/hook/lua/system/Blueprints.lua
+++ gamedata/LOUD_Misc/mods/Enhanced Static HP/hook/lua/system/Blueprints.lua
@@ -1,96 +1,39 @@
 do
 	local OldModBlueprints = ModBlueprints
+
 	function ModBlueprints(all_blueprints)
+
 		OldModBlueprints(all_blueprints)
 		
 		local modConfig = {}
+
 		for _, v in __active_mods do
 			if v.uid == 'b8d941e131multiplier' then
 				modConfig = v.config
 				break
 			end
 		end
+        
+        if modConfig then
 
-        LOG("MOD Enhanced Static HP Multipler set to "..repr(modConfig['Multiplier']))
+            LOG("MOD Enhanced Static HP Multipler set to "..repr(modConfig['Multiplier']))
+        
+            local multiplier = tonumber(modConfig['Multiplier'])
 
-		if modConfig['Multiplier'] == '1.25' then
-			for id, bp in all_blueprints.Unit do
-				if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
-					if type(bp.Defense.Health) == 'number' then
-						bp.Defense.Health = bp.Defense.Health * 1.25
-					end
-					if type(bp.Defense.MaxHealth) == 'number' then
-						bp.Defense.MaxHealth = bp.Defense.MaxHealth * 1.25
-					end
-					if type(bp.Defense.Shield.ShieldMaxHealth) == 'number' then
-						bp.Defense.Shield.ShieldMaxHealth = bp.Defense.Shield.ShieldMaxHealth * 1.25
-					end
-				end
-			end
-		end
+            for id, bp in all_blueprints.Unit do
 
-		if modConfig['Multiplier'] == '1.5' then
-			for id, bp in all_blueprints.Unit do
-				if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
-					if type(bp.Defense.Health) == 'number' then
-						bp.Defense.Health = bp.Defense.Health * 1.5
-					end
-					if type(bp.Defense.MaxHealth) == 'number' then
-						bp.Defense.MaxHealth = bp.Defense.MaxHealth * 1.5
-					end
-					if type(bp.Defense.Shield.ShieldMaxHealth) == 'number' then
-						bp.Defense.Shield.ShieldMaxHealth = bp.Defense.Shield.ShieldMaxHealth * 1.5
-					end
-				end
-			end
-		end
-		
-		if modConfig['Multiplier'] == '1.75' then
-			for id, bp in all_blueprints.Unit do
-				if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
-					if type(bp.Defense.Health) == 'number' then
-						bp.Defense.Health = bp.Defense.Health * 1.75
-					end
-					if type(bp.Defense.MaxHealth) == 'number' then
-						bp.Defense.MaxHealth = bp.Defense.MaxHealth * 1.75
-					end
-					if type(bp.Defense.Shield.ShieldMaxHealth) == 'number' then
-						bp.Defense.Shield.ShieldMaxHealth = bp.Defense.Shield.ShieldMaxHealth * 1.75
-					end
-				end
-			end
-		end
-		
-		if modConfig['Multiplier'] == '2.0' then
-			for id, bp in all_blueprints.Unit do
-				if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
-					if type(bp.Defense.Health) == 'number' then
-						bp.Defense.Health = bp.Defense.Health * 2
-					end
-					if type(bp.Defense.MaxHealth) == 'number' then
-						bp.Defense.MaxHealth = bp.Defense.MaxHealth * 2
-					end
-					if type(bp.Defense.Shield.ShieldMaxHealth) == 'number' then
-						bp.Defense.Shield.ShieldMaxHealth = bp.Defense.Shield.ShieldMaxHealth * 2
-					end
-				end
-			end
-		end		
+                if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
 
-		if modConfig['Multiplier'] == '2.5' then
-			for id, bp in all_blueprints.Unit do
-				if (bp.Physics.MotionType or 'RULEUMT_None') == 'RULEUMT_None' then
-					if type(bp.Defense.Health) == 'number' then
-						bp.Defense.Health = bp.Defense.Health * 2.5
-					end
-					if type(bp.Defense.MaxHealth) == 'number' then
-						bp.Defense.MaxHealth = bp.Defense.MaxHealth * 2.5
-					end
-					if type(bp.Defense.Shield.ShieldMaxHealth) == 'number' then
-						bp.Defense.Shield.ShieldMaxHealth = bp.Defense.Shield.ShieldMaxHealth * 2.5
-					end
-				end
-			end
-		end
-	end
+                    if type(bp.Defense.Health) == 'number' then
+                        bp.Defense.Health = bp.Defense.Health * multiplier
+                    end
+
+                    if type(bp.Defense.MaxHealth) == 'number' then
+                        bp.Defense.MaxHealth = bp.Defense.MaxHealth * multiplier
+                    end
+
+                end
+            end
+        end
+    end
 end
