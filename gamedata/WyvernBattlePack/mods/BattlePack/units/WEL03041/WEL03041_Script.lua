bbff32ce85813cb9585fe80ecbc4b312aeceb45d
Sprouto1 <loudman@rogers.com>
2025-09-06 12:57:55 -0400

    Wyvern for LOUD - Walrus
    Adjusted the water transition, and the weapon handling when underwater.   Unit was trying to target from underwater, resulting in stopping movement when travelling underwater.
    
    Revised turning rate (much too high) and acceleration (it's a big heavy gun).
    
    Reduced firing tolerance, and randomness, increased repack time and added seabed wreckage.

--- gamedata/WyvernBattlePack/mods/BattlePack/units/WEL03041/WEL03041_Script.lua
+++ gamedata/WyvernBattlePack/mods/BattlePack/units/WEL03041/WEL03041_Script.lua
@@ -4,13 +4,34 @@ local TIFArtilleryWeapon = import('/lua/terranweapons.lua').TIFArtilleryWeapon
 
 WEL03041 = Class(TLandUnit) {
     Weapons = {
-        MainGun = Class(TIFArtilleryWeapon) {
-        }
+        MainGun = Class(TIFArtilleryWeapon) {}
     },
 	
 	OnCreate = function(self)
         TLandUnit.OnCreate(self)
         self:HideBone('AATurret_Yaw', true)
+    end,
+    
+    OnLayerChange = function(self,new,old)
+    
+        TLandUnit.OnLayerChange(self,new,old)
+        
+        local wep = self:GetWeaponByLabel('MainGun')
+        
+        if not wep then
+            return
+        end
+        
+        if new == 'Land' then
+        
+            wep:SetWeaponEnabled(true)
+            
+        else
+        
+            wep:SetWeaponEnabled(false)
+            
+        end
+    
     end,
 	
 	OnStopBeingBuilt = function(self,builder,layer)
