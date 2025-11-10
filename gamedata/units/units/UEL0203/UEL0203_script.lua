8b421a4c9c5f4c2bd4e60cde998c130883397de7
Sprouto1 <loudman@rogers.com>
2025-11-09 20:22:33 -0500

    UNITS - UEF T2 Hover
    As with other UEF Hover units, gains depth charge

--- gamedata/units/units/UEL0203/UEL0203_script.lua
+++ gamedata/units/units/UEL0203/UEL0203_script.lua
@@ -2,10 +2,12 @@ local THoverLandUnit = import('/lua/defaultunits.lua').MobileUnit
 
 local TRiotGunMuzzleFxTank = import('/lua/EffectTemplates.lua').TRiotGunMuzzleFxTank
 local TDFRiotWeapon = import('/lua/terranweapons.lua').TDFRiotWeapon
+local AANDepthChargeBombWeapon      = import('/lua/aeonweapons.lua').AANDepthChargeBombWeapon
 
 UEL0203 = Class(THoverLandUnit) {
     Weapons = {
         Riotgun01 = Class(TDFRiotWeapon) { FxMuzzleFlash = TRiotGunMuzzleFxTank },
+        DepthCharge = Class(AANDepthChargeBombWeapon) {},
     },
 }
 
