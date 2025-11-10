0f597d82e0bf91cc6771806dcc75ce77502c9c5a
Sprouto1 <loudman@rogers.com>
2025-11-09 20:23:01 -0500

    BO for LOUD - Aeon T3 Wraith
    Gains depth charge

--- gamedata/BlackOps/mods/BlackOpsUnleashed/units/BAL0310/BAL0310_Script.lua
+++ gamedata/BlackOps/mods/BlackOpsUnleashed/units/BAL0310/BAL0310_Script.lua
@@ -2,11 +2,13 @@ local AHoverLandUnit = import('/lua/defaultunits.lua').MobileUnit
 
 local Disruptor = import('/lua/aeonweapons.lua').ADFDisruptorWeapon
 local Cannon = import('/lua/aeonweapons.lua').ADFCannonQuantumWeapon
+local AANDepthChargeBombWeapon      = import('/lua/aeonweapons.lua').AANDepthChargeBombWeapon
 
 BAL0310 = Class(AHoverLandUnit) {
     Weapons = {
         MainGun = Class(Disruptor) {},
         SideGuns = Class(Cannon) {},
+        DepthCharge = Class(AANDepthChargeBombWeapon) {},
     },
 }
 
