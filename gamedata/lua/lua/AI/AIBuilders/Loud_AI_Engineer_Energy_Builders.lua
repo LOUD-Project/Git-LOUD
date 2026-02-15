0d9efa2c0c01fa46e6f9f97c751c76a88fafbe07
Mofunkles <danseddon@hotmail.co.uk>
2026-02-15 19:37:08 +0000

    Revert ACU mass extractor changes and tweak energy requirements

--- gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Energy_Builders.lua
+++ gamedata/lua/lua/AI/AIBuilders/Loud_AI_Engineer_Energy_Builders.lua
@@ -558,7 +558,7 @@ BuilderGroup {BuilderGroupName = 'Engineer Mass Energy Construction', BuildersTy
         BuilderConditions = {
 			{ LUTL, 'NoBaseAlert', { 'LocationType' }},
             
-			{ EBC, 'GreaterThanEconStorageCurrent', { 300, 0 }}, -- higher cost to prevent hurting early economy
+			{ EBC, 'GreaterThanEconStorageCurrent', { 300, 500 }}, -- higher cost to prevent hurting early economy
             
             { EBC, 'LessThanEnergyTrendOverTime', { 30 }},
             
