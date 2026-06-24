a50c5e25831412f0287b5f39a204d01007d84f9c
Sprouto1 <loudman@rogers.com>
2026-06-24 13:52:36 -0400

    BrewLAN for LOUD - SSB2121
    T1 Amphibious PD

---
+++ gamedata/BrewLAN_LOUD/mods/BrewLAN_LOUD/units/SSB2121/SSB2121_Script.lua
@@ -0,0 +1,6 @@
+SSL2121 = Class(import('/lua/seraphimunits.lua').SStructureUnit) {
+    Weapons = {
+        MainGun = Class(import('/lua/seraphimweapons.lua').SDFOhCannon) {}
+    },
+}
+TypeClass = SSL2121
